d814de4a1c03317102f399537c5edeb16cc650e2
이수민 <suminjeff@gmail.com>
2024-01-19 17:38:52 +0900

    initial project

---
+++ front-end/mogul/src/app/layout.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7b7da14644f8ee65a1a0b5308fd5a8978706983559eb0395a1d5b5b66ef5706d
+size 378
---
+++ front-end/mogul/src/app/page.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8f059c751dc4f3bf99c22c4acca3deabdeb1516ec788116a095cb14999271126
+size 2765

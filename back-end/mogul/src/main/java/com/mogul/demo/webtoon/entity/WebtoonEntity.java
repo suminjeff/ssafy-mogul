016f4e06e59ec9f72d22ad3b735086a19c317ac9
Sieun Kim <sieun043@gmail.com>
2024-01-21 16:53:36 +0900

    feat(webtoon):Create WebtoonEntity.java
    Former-commit-id: 0938550f804ce4936f74cb73812f18996557d37c

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonEntity.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40b572bb110931cf9ec1842279f8971a6ea1846b778907a6eb22eb21712c7b51
-size 1735
+oid sha256:1e867cb8ec0af87f08789e8fcc9dda9d1fe6f23507c2a944f172c3e481232833
+size 1824

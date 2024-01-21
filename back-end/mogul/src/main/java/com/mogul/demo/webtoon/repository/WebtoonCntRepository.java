14df718c638ac8877a7cdcd9f436c74203d840a5
Sieun Kim <sieun043@gmail.com>
2024-01-21 17:22:07 +0900

    feat(webtoon):Update WebtoonCntRepository.java
    Former-commit-id: 79c64ef5c82b98208b6510af0f1dbe7bee621b39

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonCntRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonCntRepository.java
@@ -1,8 +1,3 @@
-package com.mogul.demo.webtoon.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WebtoonCntRepository {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a74ec299dc2c0bbd5eaecbc84374cfdf9f0b4d99da78e5061aa65a739bc2b9ad
+size 557

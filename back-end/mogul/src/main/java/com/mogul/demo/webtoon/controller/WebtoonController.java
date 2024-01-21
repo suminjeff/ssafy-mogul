e05e8b52a7c75f7e7884417612c2643d64283e89
Sieun Kim <sieun043@gmail.com>
2024-01-21 18:22:32 +0900

    feat(webtoon):Update WebtoonController.webtoonListAll
    Former-commit-id: 5efd0281f457e0a739b44c4e954ffbae6eb2df01

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/controller/WebtoonController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/controller/WebtoonController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b27e060d61a51918dea4cb74685782bf6e37aa1bfe51392227ee3e74184e10b3
-size 810
+oid sha256:3d1c64cda5e58e7b28c13e1d9ab46c16f405b85cdcdb15a51d11cb51c12fcbee
+size 1084

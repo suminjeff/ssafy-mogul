3b10a34287f46b3dd9ced77fe5fbda49bfdc24d8
Sieun Kim <sieun043@gmail.com>
2024-01-21 18:22:47 +0900

    feat(webtoon):Update WebtoonRepository.findAllByTitle

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf72f236e8c836bd9ec6da0cf699e853a855a5ec78bdbce6dfd730d07931825d
-size 591
+oid sha256:9c1b95b635dfd22c2f3d886a0f5789fd699399bd16cb2ed3b59fcd663a922410
+size 732

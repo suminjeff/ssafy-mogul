1b15fd1865bdced569bffe9ec4ed39ed633cf62d
Sieun Kim <sieun043@gmail.com>
2024-01-21 18:50:35 +0900

    feat(webtoon):Modify WebtoonCntRepository.find to WebtoonCntRepository.findMain

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonCntRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonCntRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a74ec299dc2c0bbd5eaecbc84374cfdf9f0b4d99da78e5061aa65a739bc2b9ad
-size 557
+oid sha256:2a35b412581f962386b98324ad78115089a913821cc994c64ac8fc81aa92758e
+size 561

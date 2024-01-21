f250e1918a2688c1c0c063fa90b6b8f13f41d34a
Sieun Kim <sieun043@gmail.com>
2024-01-21 16:53:58 +0900

    feat(webtoon):Create WebtoonService

---
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:39e117984ecba1b41f0c28008a5e6de18f257bc68732c274b0aeac097ed0d53f
+size 78
---
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonServiceImpl.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6811031e3299135fc8ed0fcf733a9b7882612c04119a09c7e6bd0cc9e76f2576
+size 159

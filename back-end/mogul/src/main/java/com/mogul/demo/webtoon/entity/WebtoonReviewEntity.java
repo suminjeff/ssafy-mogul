cecccc75aada4950094a03482b13691446928bdf
Sieun Kim <sieun043@gmail.com>
2024-01-21 21:17:35 +0900

    feat(webtoon):Add WebtoonReviewEntity and WebtoonReviewRepository

---
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonReviewEntity.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ca56406c119541eeea49c0623b2eeb4049682d1e0bae0c88d36eb322f5dd2a0e
+size 77
---
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonReviewRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5680adc29ff7309c80f86e7b3cd004fd99eb58dea5f37e67772c3206d7375be8
+size 90

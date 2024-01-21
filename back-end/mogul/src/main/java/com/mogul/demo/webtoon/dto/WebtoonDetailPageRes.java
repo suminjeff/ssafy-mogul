cf7025e6ed6ac3595915c6c367bf951a03774e6b
Sieun Kim <sieun043@gmail.com>
2024-01-21 20:39:54 +0900

    feat(webtoon):Update WebtoonDetailPageRes.java

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/dto/WebtoonDetailPageRes.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/dto/WebtoonDetailPageRes.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83e49994ec98a56964eecdba66b9747db2d1a82989df16f29f54fe83fff47d01
-size 247
+oid sha256:71e34f413598dfc8adbf033eb9fbb4f04bee6c3c945ba814cbe1c383afdffcfa
+size 308

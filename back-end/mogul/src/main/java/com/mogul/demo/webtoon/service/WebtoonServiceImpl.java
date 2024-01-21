cbd27d05ba4bb2557df0a6164ba43e00ff964722
Sieun Kim <sieun043@gmail.com>
2024-01-21 17:09:42 +0900

    feat(webtoon):Update WebtoonService
    Former-commit-id: 02a50442e402087957c0ae299dfaef430deba7f9

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonServiceImpl.java
@@ -1,7 +1,3 @@
-package com.mogul.demo.webtoon.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class WebtoonServiceImpl implements WebtoonService{
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:fada3fbf590295dd3a81522763038763214bdfeb1a746dd1ba50149d42bcd7ef
+size 964

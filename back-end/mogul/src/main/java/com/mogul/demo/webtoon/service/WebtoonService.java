05c60e438d050efba67045fe638447bf3a6e68d2
Sieun Kim <sieun043@gmail.com>
2024-01-21 20:39:24 +0900

    feat(webtoon):Update WebtoonService.findWebtoonDetail

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d713be10f49313ef6ac6923e7da3035752dee313a030c5feca76aaee608f283
-size 470
+oid sha256:b47bf5d20877a7c724b84823f8dfdfb7fe971c19523b13f225d5feb8d86278d9
+size 615

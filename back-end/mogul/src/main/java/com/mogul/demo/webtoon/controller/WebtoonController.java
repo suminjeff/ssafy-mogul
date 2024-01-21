9c0dec285dd949034f3b2d88d347c31a259e70c9
Sieun Kim <sieun043@gmail.com>
2024-01-21 18:53:05 +0900

    feat(webtoon):Fix 오타

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/controller/WebtoonController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/controller/WebtoonController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4a0f60a3066b672f85592e26a9883fba698a9e3cc1dfb72f2445797125a9321
+oid sha256:b171df1c684dfca09750609cdb8aad04cf848b5d1a1b2c880f9f7aa0d6602dd9
 size 1225

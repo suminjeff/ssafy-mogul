4bd76ed135ee7d395ec8c82e171fb035b5fa5742
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-01-22 17:48:29 +0900

    modify_css
    Former-commit-id: 375643aa233745d2234dfa1777e928148ff44bfc

--- front-end/mogul/src/components/Navbar.js
+++ front-end/mogul/src/components/Navbar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0480ed5c9afb465d8b77f34e5b1d35fd957a62f69912008e74195d87ed295e5f
-size 698
+oid sha256:172782fde1af7127cf1a4084fad118072a8d1057e65438078cfb10a3fc1242c3
+size 916

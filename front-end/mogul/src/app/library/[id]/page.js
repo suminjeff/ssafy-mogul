5beb04300adbece014d5cc4f3fcb70d0ed950071
이수민 <suminjeff@gmail.com>
2024-01-22 23:32:58 +0900

    feat(routing): 웹툰, 서재 라우팅용 기본 페이지명 pages -> page 로 변경

---
+++ front-end/mogul/src/app/library/[id]/page.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3f092896761e85fe929918fbc6a743ae88d4a420a55d56082c6f9b3fda0983ac
+size 109
---
+++ front-end/mogul/src/app/library/page.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a10deaafa46d559f6c1cf4ddd234955f311102eebc4a28db22988a62c1dfbd86
+size 96

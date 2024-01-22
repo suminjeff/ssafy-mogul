524bfd2fce3867a3f8bb9a8fa29e1d0e610dcef2
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-01-22 16:13:15 +0900

    feat(login/signup):소셜 로그인/회원가입 화면 구현
    Former-commit-id: 385cfb70e5c3619a005808a64d5306a1a11e6877

---
+++ front-end/mogul/src/app/user/login/Login.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4e89c8091985dda6f93405f24cad53f2001b2213456a60112052f83417062659
+size 887
---
+++ front-end/mogul/src/app/user/login/page.js
@@ -0,0 +1,8 @@
+import Login from "./Login";
+export default function Form() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}

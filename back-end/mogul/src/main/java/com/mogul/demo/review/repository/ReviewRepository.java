9543da8120265aa5272635c3e6b017c6949b35e0
Sieun Kim <sieun043@gmail.com>
2024-01-22 17:29:06 +0900

    feat(webtoon): Update ReviewRepository.findByWebtoonId
    Former-commit-id: aea18d6828e15efee09cc03cd62f01b3205bc8e8

--- back-end/mogul/src/main/java/com/mogul/demo/review/repository/ReviewRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/review/repository/ReviewRepository.java
@@ -1,9 +1,3 @@
-package com.mogul.demo.review.repository;
-
-import com.mogul.demo.review.entity.ReviewEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a1b6bf2c6b622437367da0d604abdf751da51881edd04d38089953de848b8e9c
+size 612

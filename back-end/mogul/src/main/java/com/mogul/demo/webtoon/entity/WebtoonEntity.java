be7124e2b3e3e4309528f20923757bd2e62013c4
Sieun Kim <sieun043@gmail.com>
2024-01-23 17:51:19 +0900

    feat(webtoon): Update API GET /webtoon
    Former-commit-id: a99b20aea51eae17dfc0de5713ff75c74bb57bed

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonEntity.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e867cb8ec0af87f08789e8fcc9dda9d1fe6f23507c2a944f172c3e481232833
-size 1824
+oid sha256:80f6d68cbd9bd1b0da5feab8bdb4847d6b38679a602e4bc7223aa1174c5a531d
+size 1775
--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:268757ed9504f5275d3f3a8af149f5bf5e9a35dd9670f0e1713ef951b3a433ff
-size 4829
+oid sha256:ebbee8290b7424c9d1231ba1e5efd33cae91e7994f891ccca26d529e310ec745
+size 4291

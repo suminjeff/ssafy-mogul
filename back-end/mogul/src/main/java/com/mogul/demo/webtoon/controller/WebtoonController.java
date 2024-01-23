0a4c35d87d9b8808af15fba38dd265d4376bbb19
Sieun Kim <sieun043@gmail.com>
2024-01-23 10:34:36 +0900

    feat(webtoon): Modify packagename
    Former-commit-id: 48d5f44aa919d4e5a6d7049701fc47ffe86b7eae

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/controller/WebtoonController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/controller/WebtoonController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7496fc501b1491523aabc5c97333226e2d2a2ebf401bf50ddfede0b11d10092
-size 1557
+oid sha256:317f4d42a1d8e1749b26c8f42c195ab7942c97b669a5010e164e1708c0680586
+size 1577
--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b47bf5d20877a7c724b84823f8dfdfb7fe971c19523b13f225d5feb8d86278d9
-size 615
+oid sha256:90b7ca6d8f13e2b75dc2415ff50b40a38e86b5395fdbaf4d9d118d5371cc3df8
+size 635

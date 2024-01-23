95e5a7ca81b2e912ed42f27d9f435161528cacad
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-01-23 22:57:37 +0900

    feat: Update Mypage/LoginButton/tailwindcss
    Former-commit-id: 2fd0e3f1a8665da1630d2a216ae5d675dd5c85c0

--- front-end/mogul/src/app/user/profile/Mypage.jsx
+++ front-end/mogul/src/app/user/profile/Mypage.jsx
@@ -1,17 +1,3 @@
-import React from "react";
-import ProfileImage from "./ProfileImage";
-import "./Mypage.module.css";
-
-export default function Mypage() {
-  return (
-    <div>
-      <div className="grid-container">
-        <div className="grid-item">
-          <ProfileImage />
-        </div>
-        <div className="grid-item">2번째칸</div>
-        <div className="grid-item">3번째칸</div>
-      </div>
-    </div>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9609c573bc705f73046165627acea02d80ada487aa8fe52a6e131f57c9895558
+size 681
--- front-end/mogul/src/app/user/profile/page.js
+++ front-end/mogul/src/app/user/profile/page.js
@@ -1,10 +1,15 @@
-import Mypage from "./Mypage";
+import Navbar from "@/components/Navbar";
+import MypageContainer from "./MypageContainer";
 
 export default function profile() {
   return (
     <div>
-      <Mypage />
-      <div>프로필 전체 페이지 </div>
+      <Navbar />
+      {/* 사용자 지정색상은 아예 따로 tailwind.config.js에서
+      처리하거나 직접하려면 style로 지정해야되나봐요 */}
+      <div class="mx-20 rounded-md" style={{ backgroundColor: "#DED0B6" }}>
+        <MypageContainer />
+      </div>
     </div>
   );
 }

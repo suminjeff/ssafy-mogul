1a1aff75cb47f83ae4ecff2e015aa6a909d84e4f
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-01-23 22:57:37 +0900

    feat: Update Mypage/LoginButton/tailwindcss
    Former-commit-id: 3e47406edee08acd249c5f867cba677d91755c56

--- front-end/mogul/src/app/user/login/Login.jsx
+++ front-end/mogul/src/app/user/login/Login.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24acf83ff6112a40bc074332b7f9ce6f066fcdec228c5a85ba971e7292d40886
-size 1490
+oid sha256:5c2c6be3bba053b95c7739fc336e1beb1096aef075c4b90f5f2d9757b728ce11
+size 1132
---
+++ front-end/mogul/src/app/user/profile/Myactivity.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5d32ad579c9d4dd634bd735562c8169a89357da7affc57a3c74241a47d3a7824
+size 115

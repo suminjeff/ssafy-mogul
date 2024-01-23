3b4fd70711dc31ffe2bb35cbf2d251de03ab191c
이수민 <suminjeff@gmail.com>
2024-01-23 14:01:56 +0900

    feat(webtoon): Modify webtoon detail page
    Former-commit-id: a36ffc933d28d3cf973210b8edceb61246bcb779

--- front-end/mogul/src/app/webtoon/[id]/page.js
+++ front-end/mogul/src/app/webtoon/[id]/page.js
@@ -1,13 +1,3 @@
-import ReviewCard from '@/components/ReviewCard'
-
-
-
-
-export default function WebtoonDetail(props) {
-  return (
-    <div>
-      웹툰 상세 페이지
-      <p>{props.params.id}</p>
-    </div>
-  )
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b9aec8e8669bb2fdb51c4b2fdc5ef8154c973363e4a2516c43f82f6099af633b
+size 1106
--- front-end/mogul/src/components/ReviewCard.jsx
+++ front-end/mogul/src/components/ReviewCard.jsx
@@ -1,15 +1,3 @@
-import React from "react";
-
-function ReviewCard({title, content, rating}) {
-  return (
-    <div style={{
-      width: "200px",
-      height: "200px",
-      backgroundColor: "black",
-    }}>
-      <h1>{ title }</h1>
-      <p>{ content }</p>
-      <p>{ rating }</p>
-    </div>
-  )
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b2016f67da68de41e9a124d7299010128b5dbc088a933d611d9e11d4459d2bd5
+size 513

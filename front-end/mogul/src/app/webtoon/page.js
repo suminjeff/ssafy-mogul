d0211d07c550599717dadf3e1b9124dd137a1dcb
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-01-23 13:24:10 +0900

    feat(navbar): Add navbar path css
    Former-commit-id: 23f82d0476a3240a1e24ea1b8cfab33b4a72548e

--- front-end/mogul/src/app/webtoon/page.js
+++ front-end/mogul/src/app/webtoon/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5ec2de6104c6bbe3d479fe8d17de2c966f749ccc20d69b9a2e60fef5685d4e4
-size 96
+oid sha256:18a183ba090fba899481cf53d8f82ec2f972fe11ee8026d32ecb6e5b8c6de1a7
+size 154

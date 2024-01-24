60160147897f2a47a9776833d2e02c1299599539
On Yoo <dhsdb02@gmail.com>
2024-01-24 13:27:53 +0900

    docs: Add api docs
    Former-commit-id: 9662decbe6e13b2caeca5943cc9fbf4c5dae223f

--- back-end/mogul/src/main/java/com/mogul/demo/board/controller/ArticleController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/controller/ArticleController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc2990defabace62d62c05e2900034f82d81a95a5ed12faa7a34d8bfc8ea9803
-size 3337
+oid sha256:9446ea03a4ec25fc690fb37895fb2557ba683103702e0be786e67e4eef25e56e
+size 3578

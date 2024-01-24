58447c64e90ff8ca1f753f57f8f44cadc5e1cd9f
이수민 <suminjeff@gmail.com>
2024-01-24 18:00:06 +0900

    feat(webtoon): Add webtoon API
    Former-commit-id: 90389a6b02c1638d1a87122a02b810cf0ee284fb

---
+++ front-end/mogul/src/app/webtoon/all/page.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a8037865df05ef9763988fc1cdb11d0faa85360080fb0a1d7c135aedc5b3981d
+size 1292
--- front-end/mogul/src/app/webtoon/page.js
+++ front-end/mogul/src/app/webtoon/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8516f0d4500f73fadb991f912280a152ee8e6bec4eff2c6325e86380e3f1837d
-size 1737
+oid sha256:0531b5c611cd353a38954852dcf0491238bbbdc37d9fc7f3d43c2ff3fc57a52b
+size 1383

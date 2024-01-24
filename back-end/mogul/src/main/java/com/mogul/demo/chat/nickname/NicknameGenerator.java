96dc5cd32f904141757ef33da1425f3858092be0
Sieun Kim <sieun043@gmail.com>
2024-01-24 17:36:36 +0900

    feat(chat):Update ChatWebSocketHandler to remove user from chatting room
    Former-commit-id: 60ae4419b0da250e0dca12929c35f54e6bbbaad2

--- back-end/mogul/src/main/java/com/mogul/demo/chat/nickname/NicknameGenerator.java
+++ back-end/mogul/src/main/java/com/mogul/demo/chat/nickname/NicknameGenerator.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89734d72043408dae47a64822c15ede172fa24a15b1fa8fd0e0513b903823d3f
+oid sha256:fd9fac7009614777321dc4151d5a07c6d1a2e1fcc75acae7572d211636dd4745
 size 1993

0ae02072d504717b4adbbe56ba4c49eec7213e26
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-01-24 13:31:15 +0900

    feat: Add review modal
    Former-commit-id: 2caf45662b04fa285048dcfd619471294df7cc8a

--- front-end/mogul/src/app/board/[id]/page.js
+++ front-end/mogul/src/app/board/[id]/page.js
@@ -1,7 +1,3 @@
-export default function Webtoon() {
-  return (
-    <div>
-      게시글 상세 페이지
-    </div>
-  )
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:3dc37797e2b055216171e0f794d4b08e5fdc51b66b425f9c44288defbde9c55e
+size 106

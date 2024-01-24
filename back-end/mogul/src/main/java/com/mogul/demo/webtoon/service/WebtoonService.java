8ecb0f69f4332a12e6ea3d36faa0a8ea47090c6c
Sieun Kim <sieun043@gmail.com>
2024-01-24 09:09:54 +0900

    feat(webtoon): Update Webtoon find all order by title
    Former-commit-id: cd56f8af558f1950d5fcb489c32dc3d06ad2d209

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonService.java
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f894ce05df73d3ed1dc57b733b31415d59415d594628331b6dcc8a465a7f4eeb
-size 882
+package com.mogul.demo.webtoon.service;
+
+import com.mogul.demo.webtoon.dto.WebtoonResponse;
+
+import java.util.List;
+
+public interface WebtoonService {
+
+    List<WebtoonResponse> findWebtoonOrderByGrade(int pageNumber, int pageSize);
+
+    List<WebtoonResponse> findWebtoonOrderByLibraryCount(int pageNumber, int pageSize);
+
+    List<WebtoonResponse> findWebtoonAll(int pageNumber, int pageSize);
+}
--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebbee8290b7424c9d1231ba1e5efd33cae91e7994f891ccca26d529e310ec745
-size 4291
+oid sha256:b3103d07c90fa2ea51c74246a52c419b90e7b1126c52afba6127d499b13e399c
+size 1688

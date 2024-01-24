d7099a3a9806ec95e9b11cbd6e4343dd8038f0d4
Sieun Kim <sieun043@gmail.com>
2024-01-24 17:04:24 +0900

    feat(chat):Add Nickname Generator
    Former-commit-id: 7d233f7a375c64a7f256d1c1f483dde8b1167b58

---
+++ back-end/mogul/src/main/java/com/mogul/demo/chat/entity/NicknamePrefixEntity.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4c8247523ac5e36ba76ef018f735afb4fe840864b6fc165aa532f9e9b516b647
+size 544
---
+++ back-end/mogul/src/main/java/com/mogul/demo/chat/nickname/NicknameGenerator.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb9ba2a1e53c065f5f4e58c6f46907515ac0851c837c8f6cde2c1c9f4b71e6f1
+size 1979

dd81b77ba477f28e524f94425fca257f888d0eb1
Sieun Kim <sieun043@gmail.com>
2024-01-24 20:54:49 +0900

    feat(chat):Add get list of Chatting history from start date to end date
    Former-commit-id: 9bd088f3d9c925ad42c21b0439c4139ded5967f1

--- back-end/mogul/src/main/java/com/mogul/demo/chat/repository/ChatMessageRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/chat/repository/ChatMessageRepository.java
@@ -1,12 +1,3 @@
-package com.mogul.demo.chat.repository;
-
-import com.mogul.demo.chat.document.ChatMessageDocument;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface ChatMessageRepository extends MongoRepository<ChatMessageDocument, String> {
-    List<ChatMessageDocument> findAllByChatRoomId(int chatRoomId);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:18c7de720db5aedceb16e22304cca8c695983e4eba84faf022c68f77b1e43956
+size 528

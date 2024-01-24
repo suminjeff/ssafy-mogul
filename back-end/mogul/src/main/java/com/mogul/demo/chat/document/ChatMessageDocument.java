b765b6d4e9f93419376692ccbad878d45ca81ec0
Sieun Kim <sieun043@gmail.com>
2024-01-24 19:56:35 +0900

    feat(chat):Add ChatMessageRepository to store chatting message
    Former-commit-id: 8b8e53d0b3abc5e14eb96141967bdf2db8e51701

---
+++ back-end/mogul/src/main/java/com/mogul/demo/chat/document/ChatMessageDocument.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e541aff9db0dff123fce85bbc3ffd734b9d29d3a6f4c30b1e4fa5d885824ecf2
+size 644
---
+++ back-end/mogul/src/main/java/com/mogul/demo/chat/repository/ChatMessageRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9a41cf02bf11d2e0c96cb649bbce1575e28ebbfee2b2e97d23f4ae7bf823aaab
+size 330

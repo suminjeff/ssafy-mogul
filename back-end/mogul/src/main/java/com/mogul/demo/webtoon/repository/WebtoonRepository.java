34ac4d45b9ac7395927d120072468de16954bdf0
Sieun Kim <sieun043@gmail.com>
2024-01-24 09:20:34 +0900

    feat(webtoon): Update Webtoon List All By Genre
    Former-commit-id: 39e48d61d136063e5e13abcb44547f4302b7c128

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51122fd3f2a3a4c9284aa773984ff77e815047ee38921342c2140f5c629c7aa2
-size 801
+oid sha256:a1d128daa070f9d9875b389c2ca0dbaefef504f6e77b2709a55e765aa8484cca
+size 762

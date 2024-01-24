bf8e951692df13722a9926a176e1c14696f3bed7
이수민 <suminjeff@gmail.com>
2024-01-24 13:59:21 +0900

    chore: Modified component_directory
    Former-commit-id: ef81caa167c83f44985a4a1053814100bec49fba

--- front-end/mogul/src/app/webtoon/[id]/page.js
+++ front-end/mogul/src/app/webtoon/[id]/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f891f0326f6692aa4a9f887469b5c38bdb53d0def5f5d87a9066c2363a8ff79
-size 2501
+oid sha256:f2743cc9d2a19ce8b3c84aa5f2ff280cd0afa4bee878e667b0a9c7d3a5b765d0
+size 2561

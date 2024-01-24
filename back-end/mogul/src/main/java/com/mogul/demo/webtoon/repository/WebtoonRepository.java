f0946377a42759d5f8cf3b04306f350f46b48477
Sieun Kim <sieun043@gmail.com>
2024-01-24 12:35:20 +0900

    feat(webtoon): Modify repository get to find
    Former-commit-id: 845dcca3ba0e62107dd20e8e921fe43e029d2ac6

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:302acd571378a394fcb8b4b30b94a9497a79c435d180e607dcd51278a810ea34
-size 850
+oid sha256:0f582d8981e68346c6f8d21b0ff9ff47570d4459d02f8a21eb469aa9cd9b63d8
+size 823

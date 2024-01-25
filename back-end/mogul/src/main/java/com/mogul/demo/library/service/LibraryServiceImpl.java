59550e507641621611661364f97e95be795c098f
Sieun Kim <sieun043@gmail.com>
2024-01-25 12:34:10 +0900

    feat(library):Add get api service to get library data
    Former-commit-id: 1ae413d6b511465f8092d0b20a21412d12ef776e

--- back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b29e2ab18136ce2568291fcee6c95912415ed861d639c0781a19ccf07d7aead
-size 1015
+oid sha256:d330b2ee9230bd3e300307365ada622b5e50f26153874516a239a3e688c2b57b
+size 2044

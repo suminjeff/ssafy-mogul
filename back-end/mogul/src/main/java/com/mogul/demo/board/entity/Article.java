e8f70abf4dd2d432d34cb93088ac810d9868737f
On Yoo <dhsdb02@gmail.com>
2024-01-25 14:31:05 +0900

    feat(board): Modify id type
    Former-commit-id: 3e1e7fde269ec8dc8a99da365d9cff620a39cbfd

--- back-end/mogul/src/main/java/com/mogul/demo/board/entity/Article.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/entity/Article.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5eb1d302ab54153430cb30f4b1f5ca41272a5cd77d8dc5f38a6a9e05d4d7c4f1
-size 2136
+oid sha256:b38961f066e8dbaa55c6bf3459f405845158196190484da2aac5b0b3f3fb7cb6
+size 2137
--- back-end/mogul/src/main/java/com/mogul/demo/board/entity/Comment.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/entity/Comment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce5f1802f2688a4606eb4ff2635e73babf6e811cff889eb3dee451066084892e
-size 1069
+oid sha256:b1ffbaa528489813cf7ea4814d6067d1128447a3941c50ee5b6058e1c2713bba
+size 1070

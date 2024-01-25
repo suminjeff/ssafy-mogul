606236f2fadf3d3423ca790d17b9fb6c546788a7
Sieun Kim <sieun043@gmail.com>
2024-01-25 14:59:33 +0900

    feat(library):Add Webtoon Library View Entity
    Former-commit-id: b8ceb95ebbf0bd4c8354977841cf2a39d2da0cae

---
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonLibraryEntity.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8fcc5041eca689268b0ed1d81509bce0ce360715986501022b3cf3f4d024d950
+size 1888
---
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/repository/WebtoonLibraryRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ccbd663435e07ac94dc56c9e3403a0a72414b4b2fed1424e6e122e455d17c678
+size 551

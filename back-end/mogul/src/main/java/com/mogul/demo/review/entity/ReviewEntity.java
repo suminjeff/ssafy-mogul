ac56b2a48a51edbccddcaf5a04f951ae254943ab
Sieun Kim <sieun043@gmail.com>
2024-01-25 16:11:07 +0900

    feat(library):Fix typo
    Former-commit-id: b7d650d2c44b10f0a6e1111f329c0f2af0259895

--- back-end/mogul/src/main/java/com/mogul/demo/review/entity/ReviewEntity.java
+++ back-end/mogul/src/main/java/com/mogul/demo/review/entity/ReviewEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bc61efe9835572b096e1c00694fc927c7ab879014a2e331d444700af589d89d
-size 1199
+oid sha256:31674eb80fcb43866a3e4718f9eead8f2fbc289e571a221f296d994d91bd2ff2
+size 1198

c801a4a81658ab830d4c82d2e048b8d624a34dc6
Sieun Kim <sieun043@gmail.com>
2024-01-25 11:08:06 +0900

    feat(library):Add Get API to get data about libraries
    Former-commit-id: 20e6d30a0249fa9ee47166a649eac6e646aabf41

--- back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryService.java
@@ -1,9 +1,3 @@
-package com.mogul.demo.library.service;
-
-import com.mogul.demo.library.dto.LibraryResponse;
-
-import java.util.List;
-
-public interface LibraryService {
-    List<LibraryResponse> findLibrariesByWebtoonId(long webtoonId, int pageNumber, int pageSize);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a5f424ae67a4794232430d8dea631d207e590df961273b39e8a7b2108c1ff8ee
+size 443
--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6452c8668ee052eb683237cf9f2ece2f54cd1c658c737bfe5f46dfc72d3c50ec
-size 608
+oid sha256:d6dee3df057a7a36e311fa32364aa87eda379fbe0faa6a6716dc35cbaa6ebeee
+size 676

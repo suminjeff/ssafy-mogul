4c749d0c05a457ed95258fbe0156988d92bde975
On Yoo <dhsdb02@gmail.com>
2024-01-26 17:04:34 +0900

    docs(comment): Add api docs
    Former-commit-id: b06f4c7d1c888a0b41df57eefa8dcef29a70db16

--- back-end/mogul/src/main/java/com/mogul/demo/board/dto/CommentReadResponse.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/dto/CommentReadResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45b2b0cec412d23e26381a93efd6ad2e550c8499fab4507cd41135d086e5a38c
-size 123
+oid sha256:c36b12e2aedc0f81149f6187e7ed907a221301dfc6bbec99e1b9818fba7e18a7
+size 220

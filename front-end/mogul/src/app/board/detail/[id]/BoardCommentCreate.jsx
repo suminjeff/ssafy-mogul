66ad347ae0dab3382f4943c0a7da481f9517f1c4
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-01-26 16:28:36 +0900

    feat:  Add Comment CRUD
    Former-commit-id: dbc08490b1d8b0151602299139c141004ef066cf

---
+++ front-end/mogul/src/app/board/detail/[id]/BoardCommentCreate.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:72f269dc688025378d23d09f907a8c04115024c47e607e09ce5d9996a29d475e
+size 1517
--- front-end/mogul/src/app/board/modify/[id]/BoardModify.jsx
+++ front-end/mogul/src/app/board/modify/[id]/BoardModify.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7235e7f107073b1668f72d21ec7d07f15191d1fa245cda0983a9dad20b22af4c
-size 1885
+oid sha256:7fcc5e3fc30e8a04be53e81797ee32e63a1c5b203ed43d9e2b1e2857f1e986e6
+size 2198

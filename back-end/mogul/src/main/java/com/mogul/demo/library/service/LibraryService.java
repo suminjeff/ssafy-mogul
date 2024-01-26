0bae48253e6bdbc7ce340b20e03cc881350afe30
Sieun Kim <sieun043@gmail.com>
2024-01-26 17:33:55 +0900

    feat(library):Add library delete api
    Former-commit-id: 7ee115f5d8fa412f0c0cabe8835f34421170edf2

--- back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3e6023544563a3fff4f4646d92d92517d2bc2ef48fb19df384c87682b1c9f11
-size 564
+oid sha256:34b790a9fc6b1cafca12a24df975fb83d6e701f64695a7059d66e0bacbece60e
+size 601

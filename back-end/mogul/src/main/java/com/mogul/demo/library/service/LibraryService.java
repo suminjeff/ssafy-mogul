c2e05e0f36cda679ad83bcd90c8b0b99e78d0287
Sieun Kim <sieun043@gmail.com>
2024-01-26 16:50:53 +0900

    feat(library):Add library add API
    Former-commit-id: 0df396c434503f31388f808f72c4aa813d9f8df0

--- back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryService.java
@@ -1,15 +1,3 @@
-package com.mogul.demo.library.service;
-
-import com.mogul.demo.library.dto.LibraryResponse;
-
-import java.util.List;
-
-public interface LibraryService {
-    List<LibraryResponse> findLibrariesByWebtoonId(long webtoonId, int pageNumber, int pageSize);
-
-    List<LibraryResponse> findLibrariesHot(int pageNumber, int pageSize);
-
-    List<LibraryResponse> findLibrariesByUserId(long userId);
-
-    LibraryResponse findLibraryById(long libraryId);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a3e6023544563a3fff4f4646d92d92517d2bc2ef48fb19df384c87682b1c9f11
+size 564
--- back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:124b65f78f020276ea3f2fdbbb595ff15a8076db48b639fa9eff2e26434839ff
-size 2128
+oid sha256:6b0ea16d6cf2d8ac65243968a2e9eb4b114ec13fcb84fc71be85f2e1f0cc9d11
+size 2632

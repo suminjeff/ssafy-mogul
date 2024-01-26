422e7e2d8ae0e0470a5ceb92468e2a38845d0074
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-01-26 16:28:36 +0900

    feat:  Add Comment CRUD
    Former-commit-id: 733b4866f5471ad9afbeddbf0ac375c0d948c06e

--- front-end/mogul/src/app/board/detail/[id]/BoardDelete.jsx
+++ front-end/mogul/src/app/board/detail/[id]/BoardDelete.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cc5c28721e42e99fed1d63b20fdfc0a82e2d5d891a87c171e045105065b960c
-size 869
+oid sha256:dce4dcf322e3611c9318b5871cdb4427dfbb0ec118b428c5c8b60f4c2aece51f
+size 863
--- front-end/mogul/src/app/board/detail/[id]/BoardDetail.jsx
+++ front-end/mogul/src/app/board/detail/[id]/BoardDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:553b0d20112139ee331ac67c6df74e734a1e58f0059245cee862a7246b9ed4a9
-size 3302
+oid sha256:f575e3882428861a606a1090342c75ac12baf7aa0a42ef25426d076cbed41112
+size 4490

9ae5d08751ef5d584aa9f79afe1dbb5fd17d2424
Sieun Kim <sieun043@gmail.com>
2024-01-27 15:03:07 +0900

    feat(library):Add Modify library name API
    Former-commit-id: 34f1418c7e336dee4b465214519bf8ea70a53bb6

--- back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8920623f837146021c41af7a7ca674997b375885fae776667ad49ad8b35fbbb
-size 4819
+oid sha256:111ade3bb076826c1abb2e3ee39d870301a02102543abb8d5043800ee8b726f1
+size 5178

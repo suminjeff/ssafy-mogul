5d1454115c4743038a13350c2da9d593386e358d
Sieun Kim <sieun043@gmail.com>
2024-01-27 14:02:38 +0900

    feat(library):Add Subscribe library API
    Former-commit-id: 89de67a6fe5196bc09c0e7984f88ce85e4d2b6a5

---
+++ back-end/mogul/src/main/java/com/mogul/demo/library/dto/SubcriptionRequest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:19509c010b58e301bfee013889ae58055f120407ced8b16e5c222cd9e60096ca
+size 626
--- back-end/mogul/src/main/java/com/mogul/demo/library/repository/LibraryRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/library/repository/LibraryRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3521e86c5c5c3dc66f4c8f5114e88bee2cc766d3c9e514c863eb1f6113e5568
-size 932
+oid sha256:e3bee7d16e461cfbdeabcb25d37f01c003eabc2f4c8250d1ab428f0ac1f5a21e
+size 1149

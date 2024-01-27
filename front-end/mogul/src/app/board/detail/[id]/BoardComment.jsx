e1d841c2fecfad85eb6fcdb8b5b2c7c90ac0a734
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-01-28 00:12:23 +0900

    fix: Modify board/comment CRUD
    Former-commit-id: 9f4d07a43b9bd36713ae0dec6f85c3861c03ed75

--- front-end/mogul/src/app/board/detail/[id]/BoardComment.jsx
+++ front-end/mogul/src/app/board/detail/[id]/BoardComment.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10aba4b35de093cd4f4ef0b7ad1aaf60171621a93c845e88d0cfaff13dffd0f4
-size 2687
+oid sha256:b59eeceb52a9c011819199bb861f9ff257cb70d790af9a5d24d530b72acb7137
+size 4570

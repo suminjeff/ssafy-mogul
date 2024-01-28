0c58c14d322d511e9ac0842581b3891461d0f02f
백천주 <dieswing@naver.com>
2024-01-28 13:22:32 +0900

    Update README.md
    Former-commit-id: 2e809603ee01fe459d6ba1a3b37f6c0a1f2dc2b7

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8706e4179cab32949874da97fbd0ab11c364b61744b420f034846107f13bbe87
-size 2576
+oid sha256:ae954cf947d8c0d45e086e7aa404473b7ee971eaa63ac52ecd285c991a0efba2
+size 3693
--- front-end/mogul/src/app/board/[id]/page.js
+++ front-end/mogul/src/app/board/[id]/page.js
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ebac8e855fd350a8e9865a9a9c2b76498cfb9f1fa69cfacd4511100b85188465
-size 203
+import BoardList from "./BoardList";
+import Navbar from "@/components/navbar/Navbar";
+
+export default function BoardDetail() {
+  return (
+    <div>
+      <Navbar />
+      <BoardList />
+    </div>
+  );
+}

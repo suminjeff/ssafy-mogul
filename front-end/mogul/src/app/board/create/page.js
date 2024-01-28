5c523cbbd1d7bb7371a219421ae21b757851f396
yeongbin05 <bigstar96115@hanmail.net>
2024-01-29 01:44:41 +0900

    feat: Add chat
    Former-commit-id: 0ddef27e88e961c37472a426f9f9b1b21e1c4255

--- front-end/mogul/src/app/board/create/page.js
+++ front-end/mogul/src/app/board/create/page.js
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:11cb83cc3136ebdcaaa9a5168d68f474e3f3f791961abc6b1e450af5e654df08
-size 208
+import BoardCreate from "./BoardCreate";
+import Navbar from "@/components/navbar/Navbar";
+
+export default function BoardWrite() {
+  return (
+    <div>
+      <Navbar />
+      <BoardCreate />
+    </div>
+  );
+}

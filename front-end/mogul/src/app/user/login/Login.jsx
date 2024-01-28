e1a138cda8b25c836ba9ab645adb0419624f064c
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-01-28 12:52:55 +0900

    fe-feature/user/normalLogin&Mypage
    Former-commit-id: c6e9e118eb889cb0a9a7beb2612f71cc0e2453f1

--- front-end/mogul/src/app/user/login/Login.jsx
+++ front-end/mogul/src/app/user/login/Login.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c2c6be3bba053b95c7739fc336e1beb1096aef075c4b90f5f2d9757b728ce11
-size 1132
+oid sha256:e3d44ca20de8d17f113328a7800f0d32de2191bb53d17583179a70954078dbb8
+size 1308

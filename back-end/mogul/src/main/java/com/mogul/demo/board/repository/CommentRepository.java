266b66c63aff8217ff7cecefa61c432ba4d3f3ab
On Yoo <dhsdb02@gmail.com>
2024-01-29 13:25:10 +0900

    feat(comment): Modify find by article id
    Former-commit-id: 37c5fd83823acb32188d4cb98ab5ccd075d98cc0

--- back-end/mogul/src/main/java/com/mogul/demo/board/repository/CommentRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/repository/CommentRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d9ce368d83a0a4db3532abaaa885671291f3ee1a726e80464ba861ce6f2c6b5
-size 762
+oid sha256:6bf7e168b69a27164642938345526c41d6b414e52abe45cf1f645a590e3ef01a
+size 824
--- back-end/mogul/src/main/java/com/mogul/demo/board/service/CommentServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/service/CommentServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:862d70950981101d78fc27ea5a419dfa190c91398e1a68b25e2955c8e750d829
-size 2235
+oid sha256:12da33b5cfc89c1eb1ad86806adf73becf25a6e2780865f1a703088ce40a6467
+size 2245

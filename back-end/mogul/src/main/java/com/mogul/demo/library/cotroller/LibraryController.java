4281082ebc465609a71581b0de2ae8b3ea071ab2
Sieun Kim <sieun043@gmail.com>
2024-01-29 15:02:18 +0900

    feat(library):Docs library controller
    Former-commit-id: 874b8361d6d98073401708e83d8dd213c77c87e1

--- back-end/mogul/src/main/java/com/mogul/demo/library/cotroller/LibraryController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/library/cotroller/LibraryController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b768476497965bad239f295243692494db5a59ed6d160c9ea7f37fa71cc1dc04
-size 7157
+oid sha256:03d475a79e8cbb3ad859f3f9b487ec73a46511f0a0293d7c588802f3307e6ab9
+size 12987

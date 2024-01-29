c1f5979b98cb357b1fe5bc6a80644da23a234fb9
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-01-29 17:42:02 +0900

    feat: Update boardDetail CSS
    Former-commit-id: 8e09c3022e7a1ebd83f1eb4ebad165db4a674a06

--- front-end/mogul/src/app/board/detail/[id]/BoardDelete.jsx
+++ front-end/mogul/src/app/board/detail/[id]/BoardDelete.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8df21445ba45388a696ef326f87ee84573ff19a6a57eeb7e2436b796fd582b2f
-size 1046
+oid sha256:204550c9711cade6b089166e98877ac97f95759bfada71b2a6a3412986bed77e
+size 1048
--- front-end/mogul/src/app/library/List/[id]/page.js
+++ front-end/mogul/src/app/library/List/[id]/page.js
@@ -1,11 +1,11 @@
 import Navbar from "@/components/navbar/Navbar";
 
-export default function LibraryDetail() {
+export default function LibraryList() {
   return (
     <div>
       <div>
         <Navbar />
-        서재 상세조회 페이지
+        서재 리스트 페이지
       </div>
     </div>
   );

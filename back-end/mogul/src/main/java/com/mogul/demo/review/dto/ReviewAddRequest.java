0ac9bfb5aa9d472e57600c4791b37798c87d55b9
Sieun Kim <sieun043@gmail.com>
2024-01-29 13:06:11 +0900

    feat(library):modify dto
    Former-commit-id: 0a4f35618e7f4c257a99c1434b1501e4880b6943

--- back-end/mogul/src/main/java/com/mogul/demo/review/dto/ReviewAddRequest.java
+++ back-end/mogul/src/main/java/com/mogul/demo/review/dto/ReviewAddRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:801c992b06124da547ebd3f1a96d168728d200e5ddbcbe0e57d27dbaaf94d94c
-size 1524
+oid sha256:ec6f6f737b61b23eedc41d25a4d5c6579b6c2576f176b2f80b05ded50ba20f17
+size 1531

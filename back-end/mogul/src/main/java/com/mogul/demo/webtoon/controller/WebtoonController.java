1b2f18a0003ce2310cdb4035b5b0882cfee6f7ad
Sieun Kim <sieun043@gmail.com>
2024-01-29 13:51:56 +0900

    feat(library):Docs webtoon controller
    Former-commit-id: 6e2172b0e31ae5a19c23fe8d37f9e11c51c0c124

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/controller/WebtoonController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/controller/WebtoonController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c971f1c731f0cd4d7c0e1dec24da33eee45607d88a2efc4d791665076e5b59b0
-size 3233
+oid sha256:18f96eaadaf6bb083ca403a9d17385124479eae84e083f13d4230826603cb2fd
+size 5462

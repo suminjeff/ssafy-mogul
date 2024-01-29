98476a406fea165b541f6b51b84953b526ed62da
신종욱 <rmm1@naver.com>
2024-01-29 17:38:31 +0900

    feat(core): Add CORS filter
    Former-commit-id: 55134f404b91a96404711b6eb008acc9343eac2e

--- back-end/mogul/src/main/java/com/mogul/demo/user/auth/config/SecurityConfig.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/auth/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4581782800dbecf03edba60f311bbe9d7aac8dc1301ac97e5e5c00434b7faaac
-size 2728
+oid sha256:f6e90396daa06995b3c7d5cc12530de876d397b50325241ab0aff998a9246cc0
+size 3668

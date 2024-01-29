188e4bf2cac35543c263f1374f96fd6cdb279695
On Yoo <dhsdb02@gmail.com>
2024-01-29 13:25:10 +0900

    feat(comment): Modify find by article id
    Former-commit-id: 2a12a5cccdae428278869bf1083ce5068ca01217

--- back-end/mogul/src/main/java/com/mogul/demo/board/controller/CommentController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/controller/CommentController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e05875581f1c12c4781d8535f1f18bafa7d4c210c5488eb6c3efca81e16627c3
-size 4638
+oid sha256:c4e57a4a5098f97cdd82475d96a986d2f631b35aa73b8daba16735a20b3b1db4
+size 4640
--- back-end/mogul/src/main/java/com/mogul/demo/board/service/CommentService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/service/CommentService.java
@@ -1,19 +1,3 @@
-package com.mogul.demo.board.service;
-
-import java.util.List;
-
-import com.mogul.demo.board.dto.CommentCreateRequest;
-import com.mogul.demo.board.dto.CommentGroupResponse;
-import com.mogul.demo.board.dto.CommentReadResponse;
-
-public interface CommentService {
-	/*
-	* todo
-	* 댓글 그룹 조회
-	* 댓글 작성
-	* 댓글 삭제
-	* */
-	List<CommentGroupResponse> findCommentList(int articleId);
-	CommentReadResponse addComment(CommentCreateRequest commentCreateRequest);
-	boolean removeComment(int id);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:37c3bd0af9069f1ea94ad056464250936ad36e183d72fa06c49fb3b4b4bc275c
+size 506

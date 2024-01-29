94da2290e80a2c6489cb5e5c3275bcd0c3933bcb
이수민 <suminjeff@gmail.com>
2024-01-29 10:36:18 +0900

    feat(signup): Add style
    Former-commit-id: 9ba1979719b8be8ede6738f9322a81ab2ed5355d

--- front-end/mogul/src/app/webtoon/[id]/page.js
+++ front-end/mogul/src/app/webtoon/[id]/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70110b9cdbfe1573a5218597c5ebf553c7ddb210a43fe308612b9f60173bb637
-size 1383
+oid sha256:2724a0d47c9531b6fee6ef904b9a842deb06aeed972eebb3d8f84c47db1d72a7
+size 1415

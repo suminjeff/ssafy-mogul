116626ca0111c7939dca40a3e5d95b6e54e3606a
Sieun Kim <sieun043@gmail.com>
2024-01-29 13:17:46 +0900

    feat(library):Add service transactional
    Former-commit-id: cbbedbf0bd1509fccf57e78836c425755fefadc4

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a8cc892adfbe12ed74bc7d0f03c026a274afb358217ae65d668986816a3545d
-size 3328
+oid sha256:81d39fb176aef779580fb6bcde286cccef90edd1f70e22245cc7939a01747b08
+size 3580

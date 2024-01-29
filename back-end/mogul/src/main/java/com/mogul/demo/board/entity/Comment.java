cf631fd6d0c9cbe2a4f4228724f12c3785df91d0
On Yoo <dhsdb02@gmail.com>
2024-01-29 13:24:00 +0900

    feat(comment): Modify comment field
    Former-commit-id: e568d6994982d0d93ef17257d29cb2d8a588ecb5

--- back-end/mogul/src/main/java/com/mogul/demo/board/entity/Comment.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/entity/Comment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1ffbaa528489813cf7ea4814d6067d1128447a3941c50ee5b6058e1c2713bba
-size 1070
+oid sha256:db00a7d98fcf7c18130a84e25cb04217bd8dff3bdbc899c6c3d1ac55ef695370
+size 1072

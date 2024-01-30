876919384575932b400cc03d8ed4b3ed74ceed9c
신종욱 <rmm1@naver.com>
2024-01-31 01:09:42 +0900

    feat(user): Add JPA auditing
    Former-commit-id: 0abc5a1000ce9d4fa89ba5b7ee50b7513239d922

--- back-end/mogul/src/main/java/com/mogul/demo/user/entity/User.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:208785f063c2f35abb66015f675a37ab97c67df956f5cf03d1e7795b17fcd68c
-size 1118
+oid sha256:bee849a4c51c584053f7f1df1824cf87eb452c9399c04ba1a587563327936344
+size 1574

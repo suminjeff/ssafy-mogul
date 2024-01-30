7cec645fd8185f29ccbc951eadcc6fbd523c2144
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-01-30 09:23:41 +0900

    chore: package install
    Former-commit-id: ef7ffd92c4ff540f23bbe205b592a3f41997dfb5

--- front-end/mogul/src/app/library/[id]/page.js
+++ front-end/mogul/src/app/library/[id]/page.js
@@ -1,7 +1,3 @@
-export default function LibraryDetail() {
-  return (
-    <div>
-      서재 상세 페이지
-    </div>
-  )
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:3f092896761e85fe929918fbc6a743ae88d4a420a55d56082c6f9b3fda0983ac
+size 109

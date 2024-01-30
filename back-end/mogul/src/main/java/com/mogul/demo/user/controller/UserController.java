a028ba02e72f9da2ff4c0f5fd7391874587a81c6
신종욱 <rmm1@naver.com>
2024-01-31 02:20:43 +0900

    refactor(core): Modify response of login and join
    Former-commit-id: 9321449be0d2ab76f8f9a5286a27a1a2064f403d

--- back-end/mogul/src/main/java/com/mogul/demo/user/controller/UserController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89f65347d96a0a66d9b4bb83cc135339655a72ddf3327ae2bffc8e5f308e14bd
-size 2227
+oid sha256:fc6c336f5668606aacd27e7f3bc59f653bbe71d1c6e123729494a507c3727aeb
+size 2993

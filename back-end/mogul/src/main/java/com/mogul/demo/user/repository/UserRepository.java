a9fe795605505932f7daf7f3afdea02551ee98a0
신종욱 <rmm1@naver.com>
2024-01-31 02:24:19 +0900

    feat(user): Delete unnessary method declaration
    Former-commit-id: 4cdd2fe77016ac11723dc58fdbd0d16efe7970c1

--- back-end/mogul/src/main/java/com/mogul/demo/user/repository/UserRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/repository/UserRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fa0dc08326b97cab5b0525959039b7c8b5dc7f2d8bb34f7045c7089b02a4591
-size 525
+oid sha256:02200a7e36951349d0ebf7b3c5741ff9c60d0e5b750c6dbe94c3d0f7ddf819de
+size 453

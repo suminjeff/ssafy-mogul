28c82b5ccb5e078e1b7d5e79843ceb670d009754
신종욱 <rmm1@naver.com>
2024-01-31 02:09:00 +0900

    docs: Add swagger docs about login
    Former-commit-id: 52b70a0770786ed7f4d9ff16201aaa9fca6d4cf3

--- back-end/mogul/src/main/java/com/mogul/demo/user/controller/UserController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9ca57729ac9c84188748b32f43e6085c84ff9043772a7e3739b14b099dd33cd
-size 1921
+oid sha256:89f65347d96a0a66d9b4bb83cc135339655a72ddf3327ae2bffc8e5f308e14bd
+size 2227

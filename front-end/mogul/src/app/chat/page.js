dbf56041cb6263ea2498b6514e3238aed861c0b0
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-01-30 13:42:19 +0900

    feat: Add chat button
    Former-commit-id: e05d79a8c2c585cc29cf51c5a25345d74f201d21

--- front-end/mogul/src/app/chat/page.js
+++ front-end/mogul/src/app/chat/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ed3b958a0ee4dd559134da10c0ee5506d6270e0585102d63fd62fcd6e6ca661
-size 666
+oid sha256:b6050e0143afd1a435d57049ae6ada9cee2a974a5348d52171292c77e0ecb588
+size 909
---
+++ front-end/mogul/src/components/chat/chat.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c483c949c03b8ddc9cf5b81e6e932a934a0327260af6b86b678b88008b5aa287
+size 880

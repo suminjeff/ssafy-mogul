9ebae5cec2a9b08e94ae9913efa7549cd6ec53b1
신종욱 <rmm1@naver.com>
2024-01-31 01:13:56 +0900

    refactor(user): Delete unnecessary setters
    Former-commit-id: 24c94895efc52a8938fc92d4e05d9d11b7deea13

--- back-end/mogul/src/main/java/com/mogul/demo/user/entity/User.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bee849a4c51c584053f7f1df1824cf87eb452c9399c04ba1a587563327936344
-size 1574
+oid sha256:68f4f308d656c3e0eda967fcdd9f66ef47e1f6bc5864a0c72216f4d0ff8c9feb
+size 1603

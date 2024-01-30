44e40317ab51b4cae744760de87d057090bd83a4
On Yoo <dhsdb02@gmail.com>
2024-01-30 13:44:35 +0900

    feat(comment): Modify comment entitiy type
    Former-commit-id: 53ff9efabc46e7c41104d19eb4cad0b7fb486bdc

--- back-end/mogul/src/main/java/com/mogul/demo/board/controller/CommentController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/controller/CommentController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4e57a4a5098f97cdd82475d96a986d2f631b35aa73b8daba16735a20b3b1db4
-size 4640
+oid sha256:b2d9b2ca9394ae74f15c18b2bd204fdb8e4d888e704617ba65ddc63c54252154
+size 4641

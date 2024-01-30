3cc6c89d55211104651be52652c071a43f65ba71
신종욱 <rmm1@naver.com>
2024-01-31 02:22:50 +0900

    feat(core): Add leave
    Former-commit-id: 654ca6ef653cc85e2c91d8091f7bca2a07a52473

--- back-end/mogul/src/main/java/com/mogul/demo/user/controller/UserController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc6c336f5668606aacd27e7f3bc59f653bbe71d1c6e123729494a507c3727aeb
-size 2993
+oid sha256:6efa72a27c0ffdf86255a041a6665abd9e40a47ef23879fbff099f43c9403b8d
+size 3599

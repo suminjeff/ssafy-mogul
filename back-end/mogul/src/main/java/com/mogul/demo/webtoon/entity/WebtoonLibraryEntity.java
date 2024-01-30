07c004392187b80116b2025f400bf5fb40210cb5
Sieun Kim <sieun043@gmail.com>
2024-01-30 14:30:00 +0900

    feat(webtoon-tag):Fix typo
    Former-commit-id: a41cdeefd54f669a2891457826376776f92a11c2

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonLibraryEntity.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonLibraryEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b697ea54619124249bacb05b528d425bb3bec3f4d45e5bc13de43f24e9c9e07c
+oid sha256:d8a7bb4dc0d5c25e8d503ddeed15f18cce8231debf3f24da9eb8ed194f1134f2
 size 1945

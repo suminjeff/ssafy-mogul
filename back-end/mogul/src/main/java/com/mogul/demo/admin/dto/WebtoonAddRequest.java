ac992f45e7acecf500a37f87b78feafb4f8251dd
Sieun Kim <sieun043@gmail.com>
2024-01-31 22:05:29 +0900

    feat(admin):fix typo
    Former-commit-id: 088ac8e328172f4e50fef5b330f6ae40b511b2e4

--- back-end/mogul/src/main/java/com/mogul/demo/admin/dto/WebtoonAddRequest.java
+++ back-end/mogul/src/main/java/com/mogul/demo/admin/dto/WebtoonAddRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60747f958e35d1ba6578270b93f8a0cfe6a5694159d68a5f16d29024ac06b514
-size 2859
+oid sha256:7830141da00a78eebea4855be11e23dbe7cfebb011f976fababdd510323cc9f4
+size 2860

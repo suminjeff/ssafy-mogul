cc24094ca92ed0e968fdca2a7a21f2d6e71f7b56
Sieun Kim <sieun043@gmail.com>
2024-01-31 13:38:42 +0900

    feat(webtoon-like):Add Transactional
    Former-commit-id: 2f97b37848c413909d8f3b3a87408b43cba9fc44

--- back-end/mogul/src/main/java/com/mogul/demo/board/dto/ArticleResponse.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/dto/ArticleResponse.java
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7161869bb2aba390eae34ab3d1901e83b8f8bec1dcb9a548d9885d3da0b3fb44
-size 160
+package com.mogul.demo.board.dto;
+
+import lombok.Data;
+
+@Data
+public class ArticleResponse {
+	private int id;
+	private String title;
+	private String content;
+}
--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonLikeServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonLikeServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6db78c964fce73fd03680568af338e0464ced7c8800026da7bc25ee350ecaab5
-size 1688
+oid sha256:4ebbb4ca1df8e3fcd3e3eff0dc7bf9d3dc880329c58516d737d17d76f7ae8a19
+size 1827

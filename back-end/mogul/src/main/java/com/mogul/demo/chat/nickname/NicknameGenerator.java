aa9ae594387eee0c2abbd040ccb8e95a997673f3
Sieun Kim <sieun043@gmail.com>
2024-01-31 16:50:18 +0900

    feat(chat):modify type
    Former-commit-id: 9e28684e2d185d4760d5f7a17fd8a635ead99760

--- back-end/mogul/src/main/java/com/mogul/demo/chat/nickname/NicknameGenerator.java
+++ back-end/mogul/src/main/java/com/mogul/demo/chat/nickname/NicknameGenerator.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb25969c8fc429253aee168acfe47882c42e3c2c22bdb27e8103683b4c386f39
-size 2036
+oid sha256:1e373f3cd46a50c63011d893e8b8a1b2018bcd70f2c103728e8b3d514eab1abe
+size 2037

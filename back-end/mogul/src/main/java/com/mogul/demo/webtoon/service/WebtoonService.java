f127820e6a1d695bcfdbdfa9ad6034badefccc11
Sieun Kim <sieun043@gmail.com>
2024-01-31 20:43:55 +0900

    feat(chat):Add admim controller and webtoon api
    Former-commit-id: ada1da2a436b18f34488884c0c407456b22829e0

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff59ffa440362bdaef2989ea6e5c6a2e25925d9802bbc6b658839ca30199fbc8
-size 976
+oid sha256:5f6fe1bd7570e53cd2743e0d829945d36f12db2dd2f8b3fa57b2d245dace7f8e
+size 1101
--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/service/WebtoonServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d17a88485519cdfe6b2ac492e78c63e59d3961109cfb96b0a43269383fef4d4
-size 3830
+oid sha256:5a825eba4f8b849424075987a8ef99e5a0cbda821f03fec9b2c56d88330396b1
+size 4005

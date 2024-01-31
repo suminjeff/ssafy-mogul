726e3a22b39482a3de4db9b4c4e8a3d66408faa4
Sieun Kim <sieun043@gmail.com>
2024-01-31 21:12:24 +0900

    feat(chat):Add admim webtoon tag service implemented
    Former-commit-id: 9ac211a6df1512312c4dfa523f5ce7ba35f15bd1

--- back-end/mogul/src/main/java/com/mogul/demo/admin/dto/WebtoonTagAddRequest.java
+++ back-end/mogul/src/main/java/com/mogul/demo/admin/dto/WebtoonTagAddRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:726965c43deff056cfa37251f45cd135f39ae30f4abb2ba3ccd80e1ab1ae1ac4
-size 608
+oid sha256:a6fd7bdd62813ea17c5f6c6aaf6e67ab57ca8343f981162707f647c6078fb59b
+size 619
---
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonTagEntity.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aa8692cabd560f525c2c032356fd6476b119463d21eaf832310e6dfcfbe26ab2
+size 425

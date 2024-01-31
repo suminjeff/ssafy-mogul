2cd10902b503113245216b28c7bf0c9abe209140
On Yoo <dhsdb02@gmail.com>
2024-01-31 16:42:22 +0900

    feat(article): Add transactional annotation
    Former-commit-id: 4247a915d3e9df4c7eb25ffa0a5b8ca434058d8e

--- back-end/mogul/src/main/java/com/mogul/demo/board/service/ArticleServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/service/ArticleServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:614aa3bf348d18db25d113fe9981a8839f7a172e4d2ce150ea7648f19e158cc3
-size 3229
+oid sha256:546ec7e7075b87dbb952d298241a2e4b0cd76a4b4a92b3697a90f59308e58a55
+size 3331

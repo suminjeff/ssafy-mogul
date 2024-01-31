b5a8da62985e8b971deea238c29a20f72fbd3221
신종욱 <rmm1@naver.com>
2024-01-31 16:52:37 +0900

    refactor(user): Modify user DTO
    Former-commit-id: f83c025c102e428bd3a245b3b59f93e5d41eb35d

--- back-end/mogul/src/main/java/com/mogul/demo/user/entity/User.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68f4f308d656c3e0eda967fcdd9f66ef47e1f6bc5864a0c72216f4d0ff8c9feb
-size 1603
+oid sha256:a0e6da66e6eab27b17ce836b4c4db6e05c7d2931703c9f9cdc33bd0f28bb3a5c
+size 1587

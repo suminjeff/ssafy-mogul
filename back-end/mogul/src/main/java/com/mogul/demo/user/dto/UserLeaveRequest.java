6f2d137c554d2e23c0e5c4c432565e13419b06ba
On Yoo <dhsdb02@gmail.com>
2024-01-31 16:56:35 +0900

    feat(article): Delete user id
    Former-commit-id: bcbe358854e1c4c1adfab6a02382f3dac55175b1

--- back-end/mogul/src/main/java/com/mogul/demo/user/dto/UserLeaveRequest.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/dto/UserLeaveRequest.java
@@ -1,10 +1,3 @@
-package com.mogul.demo.user.dto;
-
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-@Getter
-public class UserLeaveRequest {
-	private final String userId;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9f86cf3c30c33284d2a189590829fc83501f7dad644e9bb42dfb6c3c03f3be5a
+size 193
--- back-end/mogul/src/main/java/com/mogul/demo/user/dto/UserLoginResponse.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/dto/UserLoginResponse.java
@@ -1,18 +1,3 @@
-package com.mogul.demo.user.dto;
-
-import com.google.gson.Gson;
-
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-@Getter
-public class UserLoginResponse {
-	private final int statusCode;
-	private final String statusMessage;
-	private final String message;
-
-	public String toJson() {
-		return new Gson().toJson(this);
-	}
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:ddf6ef745b9c8ade652d92fd0ac31f320c5a437a59711c5a84fd8d8142c09854
+size 357

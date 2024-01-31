f1942aecb4bd3a96e78bf587a92d88b59245f964
On Yoo <dhsdb02@gmail.com>
2024-01-31 16:42:44 +0900

    feat(board): Modify url mapping
    Former-commit-id: 0ce141e14bcdee47c7b0facee1cfbbff4eb72dc4

--- back-end/mogul/src/main/java/com/mogul/demo/board/controller/ArticleController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/controller/ArticleController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e59a76b0d653a95479893dc0fefce7d4c3e0df49548fc780e654d70184553fdc
-size 3619
+oid sha256:772e78228c80465f0e077904713b4f527e5605cdeb8601d38482fb6f1ebddc28
+size 3623

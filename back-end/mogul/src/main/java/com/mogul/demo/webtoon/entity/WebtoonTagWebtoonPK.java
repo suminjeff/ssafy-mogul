7561fa567f893472761d35ce38404c82895771b5
신종욱 <rmm1@naver.com>
2024-01-31 12:58:09 +0900

    refactor(auth): Modify space and user principal role parameter
    Former-commit-id: b5f77ced064b058f39e43d0d7a3da94e87202d54

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonTagWebtoonPK.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonTagWebtoonPK.java
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:996dbc3507223bfea028491163c0039fe049eccefd26b0645ce772331b8381e4
-size 309
+package com.mogul.demo.webtoon.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WebtoonTagWebtoonPK implements Serializable {
+    private long id;
+    private long tagId;
+}

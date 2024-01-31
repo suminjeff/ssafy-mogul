1bcf47be6233e8b2b187429c5aa6b62772ad17d8
Sieun Kim <sieun043@gmail.com>
2024-01-31 15:11:18 +0900

    feat(webtoon-like):Modify startDate of webtoon entity and dto to String
    Former-commit-id: 9cc58eae72d0d64e4f538e30ba53879eed90fd75

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonCountEntity.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonCountEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d306551d3eaf02d1dc6bd811fe8b50e28d91c26c4196a9a4ea39c2b44f868ca
-size 1883
+oid sha256:5838c1cfa88479f5a2543752b8e758bc308fa56ba910fdf19a752dd562099d09
+size 1880

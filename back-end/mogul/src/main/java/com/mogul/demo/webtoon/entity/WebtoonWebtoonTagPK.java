394ffc9a87f0f2f94fa1382d63eab95027897d3d
Sieun Kim <sieun043@gmail.com>
2024-01-31 21:39:33 +0900

    feat(admin):fix typo
    Former-commit-id: 628ed0f4f89cdb8e4878e93b7d2b10ae22a5af99

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonWebtoonTagPK.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonWebtoonTagPK.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4bcfb8ffcb44b60e7d749cfc24b3f464643058bd5f74e7e2f98e59b34655e8c
-size 339
+oid sha256:44710a69293c817379e80edfaf931f0120e19d153db2c4c611b57d068d2e7043
+size 340

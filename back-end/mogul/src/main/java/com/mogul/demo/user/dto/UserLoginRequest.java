5737f8d64229c12b2181596b30a4fa18d543dda6
Sieun Kim <sieun043@gmail.com>
2024-01-31 15:58:11 +0900

    feat(config):add column default value false
    Former-commit-id: ba9db3ceea89f8c87b99e488c3bacfdca6e3fb64

--- back-end/mogul/src/main/java/com/mogul/demo/user/dto/UserLoginRequest.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/dto/UserLoginRequest.java
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a57216133e4bfb4d91c8c211d33e779bc9c8834d20cb99406df3e6cefd4dd50f
-size 302
+package com.mogul.demo.user.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class UserLoginRequest {
+	@NotNull
+	private final String username; //email
+	@NotNull
+	private final String password;
+}
--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonEntity.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/entity/WebtoonEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5810a4930821de2cfca6b78c4f7957fed1e51ad19606dba4f77dd896880d3bc4
-size 1718
+oid sha256:1b45e13585e120a3c62e40b05630ca4017263bb5a969ad1ae84631ae1c4fc3df
+size 1812

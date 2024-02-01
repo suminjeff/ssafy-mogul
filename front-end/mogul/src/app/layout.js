02baaaf8c6724c05c7b23247a6c7e47d374c976a
이수민 <suminjeff@gmail.com>
2024-02-01 14:52:50 +0900

    feat(footer): Add footer
    Former-commit-id: e99fda4ec925a36bee6853173e65356e5814f8d0

--- front-end/mogul/src/app/layout.js
+++ front-end/mogul/src/app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-
+import Footer from "@/components/footer/Footer";
 export const metadata = {
   title: "mogul",
   description: "모두의 굴, mogul",
@@ -8,7 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ front-end/mogul/src/components/footer/Footer.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0add325d0f3e434f731027558a067aa2f390c8d3b976c4bcf498da8a71c3e3c1
+size 1638

f555ab347640b7718b1360eee21840aa9d41dcff
On Yoo <dhsdb02@gmail.com>
2024-02-01 14:54:44 +0900

    feat(board): Modify user id to user entity
    Former-commit-id: 5616e20ee38d53e8c9ff5b47fa74a1845e4c8ae6

--- back-end/mogul/src/main/java/com/mogul/demo/board/dto/CommentRequest.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/dto/CommentRequest.java
@@ -1,3 +1,17 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d1e6f40b9e8028702505dd7e1e8ab194d8f3ecf57637712b44efdaaaaa036458
-size 849
+package com.mogul.demo.board.dto;
+
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class CommentRequest {
+
+	@NotBlank(message = "내용은 비어있을 수 없습니다")
+	@Size(max=150,message = "최대 150자까지 작성 가능합니다")
+	private String content; // 댓글 내용
+
+}
--- back-end/mogul/src/test/java/com/mogul/demo/MogulApplicationTests.java
+++ back-end/mogul/src/test/java/com/mogul/demo/MogulApplicationTests.java
@@ -1,13 +1,3 @@
-package com.mogul.demo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MogulApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b1e6fef14eaec150c7006dd139e2712e8253be1dcc4a0b0a16626fd02de52de3
+size 205

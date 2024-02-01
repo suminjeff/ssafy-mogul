2d3dac1aa57a88887b7a1ffa471695c108c5173c
On Yoo <dhsdb02@gmail.com>
2024-02-01 09:42:59 +0900

    docs(article): Add swagger
    Former-commit-id: 6b10e026268e917d5aa720f6fb4b24744866f4f7

--- back-end/mogul/src/main/java/com/mogul/demo/board/controller/ArticleController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/controller/ArticleController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:772e78228c80465f0e077904713b4f527e5605cdeb8601d38482fb6f1ebddc28
-size 3623
+oid sha256:b98bf51f7e51c7c194a0db5f6af1ca78ceca2d96d2eaac18a12ce45c1c3d2a38
+size 5717

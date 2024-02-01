38b12732c67f1673099bee97819a7f61a2a503d7
이수민 <suminjeff@gmail.com>
2024-02-01 17:45:48 +0900

    feat(user): Modify user signup
    Former-commit-id: 65fd1431461b6f2bfbfa649c62e0659d7a1b550d

--- front-end/mogul/src/app/user/login/NormalLogin.jsx
+++ front-end/mogul/src/app/user/login/NormalLogin.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ae0a008298ae888e08a6010460dedfe97b732dd1d9a6f2cd82057976445bd7d
-size 3619
+oid sha256:f0f8260a149ef9f61246b1e54e8d9d30a488c5bd462347e20e28895ae015dd8c
+size 3629

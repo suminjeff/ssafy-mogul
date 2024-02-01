70de68168514b457806bf130321101644f5d901e
Sieun Kim <sieun043@gmail.com>
2024-02-01 09:43:09 +0900

    feat(library):Fix service and add transactional
    Former-commit-id: e7d1b7be510236154b8edc44eff2cd33462b4cb0

--- back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b6438856082d096524b3cfef1906ab4bd995c60a302c802445d5ba9c2ff3ea5
-size 5236
+oid sha256:bbdb41bc05cebdbaf42cca4d2d7aa49a7bd101837a020406af19d55b17f63ade
+size 5515

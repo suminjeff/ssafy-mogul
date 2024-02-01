bde9b59f628e5cda8026466788759455263d95a2
Sieun Kim <sieun043@gmail.com>
2024-02-01 14:14:13 +0900

    feat(library):Fix Typo
    Former-commit-id: bc80085cee910fb2bf24b48c2616cd9ed08e1bac

--- back-end/mogul/src/main/java/com/mogul/demo/library/entity/LibraryEntity.java
+++ back-end/mogul/src/main/java/com/mogul/demo/library/entity/LibraryEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29b5e46d67d961d542fcd4cc97c8a406ab6e564259a736040d4461aba518d12b
-size 1133
+oid sha256:faf4562832d5b6901376e420e98dc34a2f8c41aa5132f11a8e331cafd0b88a44
+size 1130

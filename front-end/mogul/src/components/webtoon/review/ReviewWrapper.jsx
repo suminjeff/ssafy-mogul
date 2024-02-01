79be8451ddebe595b2fa0fb5f9d3f9f69a5633c5
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-01 15:29:14 +0900

    feat: Modify chat
    Former-commit-id: 1bdec582f2d6258538e280aa18d96c693134741e

--- front-end/mogul/src/components/webtoon/review/ReviewWrapper.jsx
+++ front-end/mogul/src/components/webtoon/review/ReviewWrapper.jsx
@@ -1,15 +1,3 @@
-import React from 'react';
-import ReviewCard from './ReviewCard';
-
-function ReviewWrapper({ reviews }) {
-  
-    return (
-      <div className='grid grid-cols-5 gap-10 mx-10'>
-        {reviews.map((review, index) => (
-          <ReviewCard key={index} review={review}/>
-        ))}
-      </div>
-    );
-}
-
-export default ReviewWrapper;
+version https://git-lfs.github.com/spec/v1
+oid sha256:9fbf9d2224370a4644658dd71063f3cdea638b70f1c7301cb9ad83628bcb9ae3
+size 333

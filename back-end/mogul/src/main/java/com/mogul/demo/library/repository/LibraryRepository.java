1659c6950ae51ac885781821e08dcb6158ef078f
김시은 <sieun043@gmail.com>
2024-02-01 10:40:31 +0900

    feat(library):Delete wrong annotations
    Former-commit-id: a8639ddd6a857994c593cc0fcb49ff1b9e270c47

--- back-end/mogul/src/main/java/com/mogul/demo/library/repository/LibraryRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/library/repository/LibraryRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bb4168eb4027f3de114de202fd4ebbca5ea5fb9190d4459b8093667b23db0a6
-size 1364
+oid sha256:529b86b87ca0f3f5397a1b59bc2d2a6cbcd8a52cd66801ac319e4e77a220a744
+size 1326

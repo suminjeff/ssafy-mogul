d0b06b35c73aba28583e4a2031c4ddd0610cc9a6
yeongbin05 <bigstar96115@hanmail.net>
2024-02-02 00:56:41 +0900

    feat: Modify Navbar WebtoonDetail Profile
    Former-commit-id: 92256a743df357bfbbae90da776ac6a30b40593d

--- front-end/mogul/src/app/webtoon/[id]/page.js
+++ front-end/mogul/src/app/webtoon/[id]/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d2358d2d605f07b3e8c15a35f580eade91f91020dc9b18848bb5a0691a12139
-size 3942
+oid sha256:0482e9f07cdd55f557622d27367cc6b17d8d93ff00402da31baac57927482c55
+size 4184
---
+++ front-end/mogul/src/components/webtoon/webtoonLibrary/WebtoonLibraryWrapper.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b4ad48276fcbfc1095fc88edead5d949583fd42502282d23a0feab06cbfa2ebf
+size 1245

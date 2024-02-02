5ec2d4f68bc6e1d9c0800b1f4c595438788c544b
이수민 <suminjeff@gmail.com>
2024-02-02 10:07:28 +0900

    feat(login): Modify login form
    Former-commit-id: e0c1ccf2eb05cb82cc6e1fbc9c03d8debe033111

--- front-end/mogul/src/app/user/login/LoginForm.jsx
+++ front-end/mogul/src/app/user/login/LoginForm.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ed2b7ebcbe95b9c0de3b985941b51cb161d7213dd9f37f886fe787bcbaf86ea
-size 3346
+oid sha256:d1da05680f2db4fd37284e38258ded8cc0ef2ea33f37ab16ff158c5a3cdd0ea5
+size 4227

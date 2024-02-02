c259203b48444438420f82a24976f2dc23bcb123
이수민 <suminjeff@gmail.com>
2024-02-02 10:31:30 +0900

    feat(login): Modify login signup UI
    Former-commit-id: 67cc7894c456c57d93b0c667964fd5f182c3596a

--- front-end/mogul/src/app/user/signup/Signup.jsx
+++ front-end/mogul/src/app/user/signup/Signup.jsx
@@ -7,7 +7,7 @@ export default function Form() {
       {/* <Signup /> */}
       <div className="flex flex-col items-center justify-center h-screen">
         <div class="text-4xl font-bold mb-8 text-center">
-          회원 정보를 입력해주세요.
+          <a href="/"className="underline underline-offset-2">mogul</a>에 가입하세요.
         </div>
         <div className="mb-2">
           <NormalSignup />

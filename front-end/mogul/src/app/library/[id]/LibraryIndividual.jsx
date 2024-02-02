7aa551e94a48be1cacb6e72ec1470acdcb6c90d0
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-02-02 13:40:02 +0900

    feat: Modify MyLibraryDetail
    Former-commit-id: 1488ab514b60566b2443078c995b0543a6c95072

--- front-end/mogul/src/app/library/[id]/LibraryIndividual.jsx
+++ front-end/mogul/src/app/library/[id]/LibraryIndividual.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:011caa3e274bfccbeb6bc336c64b20c944102ddbf9a857a583732a915651ae2d
-size 2919
+oid sha256:21d38a2432a982734c93904d66248c100ba239147923cda62a6a17941e96a02f
+size 3582

17536f6408df5cf6eacbf53d3ef8168d4550f647
On Yoo <dhsdb02@gmail.com>
2024-02-02 11:10:16 +0900

    feat(article): Modify add article
    게시글 생성과정에서 필요한 유저정보를 넣어주는 과정을 추가함
    
    feat(user): Add find user by id
    
    
    Former-commit-id: 46ef97fd1d73d6101163a8bd8cab8ee68375e6b5

--- back-end/mogul/src/main/java/com/mogul/demo/library/entity/LibraryWebtoonEntity.java
+++ back-end/mogul/src/main/java/com/mogul/demo/library/entity/LibraryWebtoonEntity.java
@@ -1,3 +1,23 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8ca69f0b67992a0ef6ab403cf09cb23585bc9c63eb06c2f4d6c181c2e91e497b
-size 423
+package com.mogul.demo.library.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "library_webtoon")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@IdClass(LibraryWebtoonPK.class)
+public class LibraryWebtoonEntity {
+    @Id
+    @Column(name = "library_id")
+    private Long libraryId;
+
+    @Id
+    @Column(name = "webtoon_id")
+    private Long webtoonId;
+}

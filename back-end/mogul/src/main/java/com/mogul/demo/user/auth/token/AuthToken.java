2206873c8b3cc692a3cddaa006044dfb891a5c22
신종욱 <rmm1@naver.com>
2024-02-04 02:59:42 +0900

    feat(auth): Update token, key generation, token provider methods
    Former-commit-id: 03928da03399dcb923c8e5de2b3009e0114c49d7

--- back-end/mogul/src/main/java/com/mogul/demo/user/auth/token/AuthToken.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/auth/token/AuthToken.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b40e166ad8fdfe82a27d750c3df4fd83d29c8dbdce0fe5720cbf786298a05d2
-size 653
+oid sha256:a67b92ced848413f2ec3959d04fd049558be2d6b8e91286c168e2ab7379d607a
+size 696
--- back-end/mogul/src/main/java/com/mogul/demo/user/auth/token/AuthTokenProviderImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/auth/token/AuthTokenProviderImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cabbcf985501d7202f323c839ffe5d9cd25ac899f815af73dca00de5bae8bf8
-size 3608
+oid sha256:f9b4abf7b17cc21f9626cdc4a3695ed909317dbc34efc45a11e44cb851681742
+size 3532

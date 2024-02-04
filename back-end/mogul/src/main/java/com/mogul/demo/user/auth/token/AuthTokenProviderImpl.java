67d952fc3a91ad2a13ef7934e146afe097cfcfa9
신종욱 <rmm1@naver.com>
2024-02-05 03:09:35 +0900

    feat(auth): Add method returns remaing time until expiry
    Former-commit-id: 05b3fa83bde600a4634f746b34fac3a4f2e73b79

--- back-end/mogul/src/main/java/com/mogul/demo/user/auth/token/AuthTokenProviderImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/auth/token/AuthTokenProviderImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3c786e0c0fe43b44951d751e1854d8482b9c5c7ac1dc1ef94f9b8c669bbb57a
-size 3371
+oid sha256:4a5941e9b9830298ad76bb910ce6e2c263368eb1da6f0df6d0105bb367223ec2
+size 3698

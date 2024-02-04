235883a60e15d66955e280e4a1df225b3924d2a9
신종욱 <rmm1@naver.com>
2024-02-05 03:05:47 +0900

    refactor(auth): Modify unauthorized message
    Former-commit-id: e470b2621540fb3240a44b64c6d3826772ccc901

--- back-end/mogul/src/main/java/com/mogul/demo/user/auth/filter/AuthTokenFilter.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/auth/filter/AuthTokenFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c12e9c7414f71619a871b80a9794fb4d978302399b83f6de69702989816a41ff
-size 2524
+oid sha256:059f81f0ad142ccc7f6d4ddac079bed99e4e323e904998b3d3b4ecaf581853d4
+size 2844

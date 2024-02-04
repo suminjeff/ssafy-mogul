da798a3d74350cbd9223cf147704c77aedcdf301
신종욱 <rmm1@naver.com>
2024-02-04 13:30:43 +0900

    refactor(auth): Update expired token exception handling and token provider
    Former-commit-id: b35829e4ed34f4c59fb0fe411938095b96ead0c3

--- back-end/mogul/src/main/java/com/mogul/demo/user/auth/token/AuthToken.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/auth/token/AuthToken.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a67b92ced848413f2ec3959d04fd049558be2d6b8e91286c168e2ab7379d607a
-size 696
+oid sha256:28a8bcf4f18f25f8d59c716c7f34ab49d33a1b6ebddfe1fc683f92334294d001
+size 815
--- back-end/mogul/src/main/java/com/mogul/demo/user/auth/token/AuthTokenProviderImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/auth/token/AuthTokenProviderImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9b4abf7b17cc21f9626cdc4a3695ed909317dbc34efc45a11e44cb851681742
-size 3532
+oid sha256:a3c786e0c0fe43b44951d751e1854d8482b9c5c7ac1dc1ef94f9b8c669bbb57a
+size 3371

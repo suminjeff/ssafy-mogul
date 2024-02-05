f2e3e3c2cb11eeb243435f642371bee896259589
신종욱 <rmm1@naver.com>
2024-02-05 17:58:27 +0900

    refactor(user): Add column,  Update method name
    Former-commit-id: de9a4a36cdf3efefb6453bae481c17e350d6a6f5

--- back-end/mogul/src/main/java/com/mogul/demo/user/entity/User.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67c1e109429c3fc7ac1cc96a6755a5f1a31cb02f15d5e4b91744ded0cbab4938
-size 1843
+oid sha256:11ec8fb9c9293198598733e4a83c75244d7934e351ce7dc65bfe5262bb7403b4
+size 1946
--- back-end/mogul/src/main/java/com/mogul/demo/user/service/UserService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28970cb472a9e2b77ef037dc8b550f2a947b3086ac6888e1d6fb924c73a46ebe
-size 969
+oid sha256:7fd04a80f5cb8035849a511a8e8bdbd51c0201e4c0614232c05331c3df92de24
+size 1015

ad38c3077d1b9930a106b34245998ea558a35093
onyoo <dhsdb02@gmail.com>
2024-02-05 13:36:41 +0900

    feat(article): Add relation entity
    Former-commit-id: 8e5f58ed6f3754249c45b363e1d9a492851964ab

---
+++ back-end/mogul/src/main/java/com/mogul/demo/board/entity/ArticleArticleTagPK.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9664d27f98948b04a1d8fd880bb8455dd07d90d2a41d245b292cad47be783893
+size 469
--- back-end/mogul/src/main/java/com/mogul/demo/board/service/ArticleTagServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/service/ArticleTagServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:114fb5d40a669a415ff8d3206b129af420fba7c0c7849e72841b233b4096243d
-size 2882
+oid sha256:48c9d68d629bf5947f29385048edf9b2a6bb92f10159854c71d7eebdbb36daef
+size 3816

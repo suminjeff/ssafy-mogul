47fc7efd9d5d25ca9dd6ce3a09de2681b632e6b5
onyoo <dhsdb02@gmail.com>
2024-02-05 13:38:42 +0900

    feat(article): Modify get article tag list logic
    Former-commit-id: f0b36eaa3f864c59818cab6bfc37093ff7c7a3c5

--- back-end/mogul/src/main/java/com/mogul/demo/board/dto/ArticleReadResponse.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/dto/ArticleReadResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9dfda6fcc7fea2412f04db623cc59b97e72b42ebe0d260bdc9201df0760d5403
-size 574
+oid sha256:970f728ce97662321db8c7d8e9d318947c0c7b7996e1ea4a8b650812f49cca24
+size 578

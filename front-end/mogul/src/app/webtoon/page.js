e80caf8421f39392643b94ae89832c6098355a39
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-02-05 17:56:37 +0900

    feat: Modify boardPagination
    Former-commit-id: 959323bc3267c694e8eb6793f3fc1350cd2c525d

--- front-end/mogul/src/app/webtoon/page.js
+++ front-end/mogul/src/app/webtoon/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19b23e54bb262f167d15ce1119c649ddb32da27bb68f17a27c78452e118f6df0
-size 935
+oid sha256:9acc41c7932c361a53478eb1ed136383a85e189149c595a4c5e4e9bc25bf496c
+size 1060

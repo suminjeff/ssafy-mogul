be5468ae859bd44bda7da948c3225ef68ccd71d0
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-05 13:51:11 +0900

    feat: Modify webtoon image
    Former-commit-id: b9e44fdbcd7399ccf79cd258bb35a28f8c94aa28

--- front-end/mogul/src/app/library/List/[id]/MyLibraryList.jsx
+++ front-end/mogul/src/app/library/List/[id]/MyLibraryList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0fc1b2168ca4e6297e96bd2cbdfbe94e65b728f29e06ad38d5a26393c218add
-size 6025
+oid sha256:03a0098b0706ac4f18c0229b01a26725d422a70a6ac396944b6a933b8940bfdc
+size 6997
--- front-end/mogul/src/app/webtoon/page.js
+++ front-end/mogul/src/app/webtoon/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19b23e54bb262f167d15ce1119c649ddb32da27bb68f17a27c78452e118f6df0
-size 935
+oid sha256:b6b5301555cf777e849e0a61fcf6f18af604837676d306850566bc2f01d47b48
+size 1051

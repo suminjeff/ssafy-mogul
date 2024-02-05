baffd2cc45b127e0da5db6fe12e02c2a698c7df2
Sieun Kim <sieun043@gmail.com>
2024-02-05 12:24:58 +0900

    feat(webtoon):Fix Query
    Former-commit-id: 158931bb4c7010e183b2d1b8e80f817e6b18f11e

--- back-end/mogul/src/main/java/com/mogul/demo/review/repository/ReviewRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/review/repository/ReviewRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9c91ab88058a7c35d88fc41dfdae5ab3d0c188f863a05256c45f09c4167e11d
-size 2050
+oid sha256:cc7634df20657543a28efbc736259ba51e8b814b05d0755872a0c82eeae72fb6
+size 2212

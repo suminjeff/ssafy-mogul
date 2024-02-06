9e511a74e02f4d9bedc69c2071129f62b649eb24
신종욱 <rmm1@naver.com>
2024-02-06 09:44:40 +0900

    feat(auth): Add get User entity from Authentication
    Former-commit-id: 79cd834a72cec34185c547c1d595885d30a59fc0

--- back-end/mogul/src/main/java/com/mogul/demo/user/service/UserService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08d8d331e471694556872ac1ddce7f22a35b453f3af0011ea0c3b5ed9c229490
-size 966
+oid sha256:7ad1ff649b305e44084dc11f3752e8e00fd9bf3c0ee0f35e16c63026563bc824
+size 991
--- back-end/mogul/src/main/java/com/mogul/demo/user/service/UserServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/service/UserServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22e7b1cc9729e9af51e67251e92e53f7deb3ea1ade22ba6f1d39f5f0806f0976
-size 4587
+oid sha256:a14d4642db83be8cd1082ab95202c6e8c36d2355a73ca21dd69fd57b5de66d45
+size 4852

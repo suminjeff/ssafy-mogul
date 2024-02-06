4aacf516f05bdccc0d9dd30f5729eeeb499945f1
Sieun Kim <sieun043@gmail.com>
2024-02-06 11:06:18 +0900

    feat(webtoon):Add get user id
    Former-commit-id: 5518985aedb02a54dd3e7d5f05eb6b3ed47b8bce

--- back-end/mogul/src/main/java/com/mogul/demo/board/repository/ArticleTagViewRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/repository/ArticleTagViewRepository.java
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e6e9a83d3451a1fb08729ad7d56db6c9f5109eeb0bb67d5643b8a33dce860524
-size 326
+package com.mogul.demo.board.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mogul.demo.board.entity.ArticleTagView;
+
+public interface ArticleTagViewRepository extends JpaRepository<ArticleTagView,Long> {
+
+	List<ArticleTagView> findArticleTagViewById(Long id);
+}

7c0d8591087af4318a5ae9a2e7ca4bbb93085b0a
이수민 <suminjeff@gmail.com>
2024-02-07 00:28:45 +0900

    feat(user): Modify login, signup details
    Former-commit-id: 1d89d8f4c6f8cfebaa57771f21c2c1f3a24c00c3

--- front-end/mogul/src/app/user/profile/BackgroundTest.jsx
+++ front-end/mogul/src/app/user/profile/BackgroundTest.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d08c48b309864a50312e157f764bd243cc74956bd78cdc08f2a7ae5455ca0570
+oid sha256:ba9cf1b60962d1e2b238a58d68029949766bac4777a85d9d562ef18b0eceb9a7
 size 655

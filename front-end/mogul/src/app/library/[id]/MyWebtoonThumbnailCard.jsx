a5fa944cb218350bd01848c685e4b2ccc9338c09
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-02-06 14:52:40 +0900

    feat: Add GetToken
    Former-commit-id: 16d562ca484348bc47a6bd54f7becb872fc31d1f

--- front-end/mogul/src/app/library/[id]/MyWebtoonThumbnailCard.jsx
+++ front-end/mogul/src/app/library/[id]/MyWebtoonThumbnailCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f7f6814610042df8701a50f834ddf436edc10134d33e84f29b9e8999c125594
-size 1280
+oid sha256:581c00c79a85cf3fba8d15fab6e98c5c2f9ccad546797300e3bd2202f8db0723
+size 1314
--- front-end/mogul/src/components/webtoon/WebtoonThumbnailCard.jsx
+++ front-end/mogul/src/components/webtoon/WebtoonThumbnailCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca9805d8f968272fb42644081c8cb9cf925c36c358cc84c86dd54acaf18e5828
-size 1249
+oid sha256:f99af016d86f390e9c8923e449229e3693c8d421bc369d516b6b2625391ac485
+size 1410

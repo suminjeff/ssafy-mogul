d850f003ee1dd7e0b217c15877aace06d2c5ece9
백천주 <dieswing@naver.com>
2024-02-07 00:40:50 +0900

    feat: Apply SessionStorage
    Former-commit-id: 1ece83d6054970e661f9a6efd0991faffd5fac7e

--- front-end/mogul/src/app/library/LibraryPopular.jsx
+++ front-end/mogul/src/app/library/LibraryPopular.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9073673ab4137d0112111e1cd97dd25bc1fcf62cd9224f1828fc417d79129af2
-size 4963
+oid sha256:8027eef29fc8cf5ceebf61af556e4eeb3ce225cdc33b03b1a1bc80bece31b706
+size 5885

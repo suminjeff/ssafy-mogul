70e1fa4e1fd20257556c2e08a0dc1ff303f8cc37
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-02-06 17:56:52 +0900

    feat(board): Add sessionStorage
    Former-commit-id: 047b4a5e6267d7009934bbfc9bdc45c6982302d4

--- front-end/mogul/src/app/board/[id]/BoardList.jsx
+++ front-end/mogul/src/app/board/[id]/BoardList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:facdb0501653ecc1736bb7725cddbe100c6401dba036beec5917c4be0da1431e
-size 8626
+oid sha256:60dbac2c3ec25c5c9577582163c06505c8af1d4cc32b08dc316c359f400ca172
+size 8880

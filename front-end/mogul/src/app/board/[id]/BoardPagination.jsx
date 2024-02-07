7f6be1c5e665e66667c80de5b485f801f7fc5214
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-07 14:57:00 +0900

    feat: modify board pagination
    Former-commit-id: 2c53247a71d15dfc66968cd28c35ecb2222d8c74

--- front-end/mogul/src/app/board/[id]/BoardPagination.jsx
+++ front-end/mogul/src/app/board/[id]/BoardPagination.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5aca7bfe0395cf296669ff6e16762530d99e9984c26e2819d2e84b52dda005f
-size 8148
+oid sha256:c8ff72a7b5272968d6f318db2b3ac3f574db29ca913c02d4a6c83fe9c0a8d5cd
+size 8116

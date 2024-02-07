fc1a08fbd693c99588665658dc815ba8a28d413d
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-02-07 14:27:44 +0900

    feat: Seperate libraryList
    Former-commit-id: 8c20e711c751505439697abf81bbca9adb825b66

--- front-end/mogul/src/app/board/[id]/BoardPagination.jsx
+++ front-end/mogul/src/app/board/[id]/BoardPagination.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7b39a03f1f40ac89996213355479ad72ef9d2942abb686f9b816834e4da2d86
-size 7485
+oid sha256:b571fb931e300e93c7eb94dbf7f185a94cd253f715f10c120d16c66eb00dccc8
+size 7511

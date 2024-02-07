6b446fafb200a2f4576ad8e68b5b2d9865735d05
On Yoo <dhsdb02@gmail.com>
2024-02-07 14:57:11 +0900

    feat(user): Add user equals method
    Former-commit-id: e9a50443d0b494cde359ec439271b2f43fe8fa1d

--- back-end/mogul/src/main/java/com/mogul/demo/user/entity/User.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:888cf1a508e50ac0dda625cb44a12e8928ab7ad0d21779eba64df2b6eea20955
-size 1952
+oid sha256:33d42c0119bc40e2de9585c54efed173428b37f343189f51a522dc5b80c20d65
+size 2352

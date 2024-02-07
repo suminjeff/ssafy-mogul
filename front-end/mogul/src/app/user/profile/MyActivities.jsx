6ef545ff6655b8a08e70527abe7d574234c47b89
이수민 <suminjeff@gmail.com>
2024-02-07 17:33:35 +0900

    feat(profile): Add profile function
    Former-commit-id: 77bd43e3fe3d23fe4c860c0f6e747971464707ec

--- front-end/mogul/src/app/user/profile/MyActivities.jsx
+++ front-end/mogul/src/app/user/profile/MyActivities.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b73513448e91aafc2658e0f8b59fad2e890b85fc57be5a0fb1cb585ac8da5ea
-size 3185
+oid sha256:0ece9a0c141c03d406589e3b784f4ac7bdce2133bda3c8f068559f3d857e9821
+size 4697
--- front-end/mogul/src/app/user/profile/preview/MyLibrary.jsx
+++ front-end/mogul/src/app/user/profile/preview/MyLibrary.jsx
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6d4c681b271dc7ec4cd4db290a37d5f9011ca6d49f836d94635b2e44d027d09c
-size 1593
+export default function MyLibrary() {
+  return (
+    <>
+    </>
+  );
+}

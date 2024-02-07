531c2c1f0495991527d66a8a246451872663fb30
신종욱 <rmm1@naver.com>
2024-02-07 12:42:34 +0900

    fix(user): Add validation for emails
    Former-commit-id: ab7f050eacc7c5756bf2872c4175cf59cd775fd9

--- back-end/mogul/src/main/java/com/mogul/demo/user/service/UserServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/service/UserServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a14d4642db83be8cd1082ab95202c6e8c36d2355a73ca21dd69fd57b5de66d45
-size 4852
+oid sha256:31b23f2f5863adcf76bdcdc2d68d9f601e73988629b5dfab91715bc7c5ee1965
+size 4840

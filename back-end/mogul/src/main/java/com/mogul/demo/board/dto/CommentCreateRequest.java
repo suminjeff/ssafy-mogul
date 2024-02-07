b115935d43031b9134a921e913a67b6e8fbdc8b4
On Yoo <dhsdb02@gmail.com>
2024-02-07 14:58:53 +0900

    feat(comment): Delete user entity
    Former-commit-id: bf0d76105fbe5582aa419aab1025510441e9f520

--- back-end/mogul/src/main/java/com/mogul/demo/board/dto/CommentCreateRequest.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/dto/CommentCreateRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6e8e32c31deace53e5fed8afa177d2220f0a706a5c5879a288586d247e0a88d
-size 625
+oid sha256:40251297b3bac85819b9bc693d4c56d8077f5cf3820048763774de479463f2c6
+size 565

ba6f2770a24914e1cc72070082a6127eff9066e6
Sieun Kim <sieun043@gmail.com>
2024-02-07 13:32:42 +0900

    feat(webtoon):remove unused import sentence
    Former-commit-id: 7652a6b9bc7cecca0a8ae7287291d8fb8af9466b

--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/controller/WebtoonController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/controller/WebtoonController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:300dacc06a05895ce5808f6621e265174eb39c482b3849ab90d9382273ee0d74
-size 8893
+oid sha256:02d8613a07f8a20fc89ee996e6cccd8f491ce07e98c83551359af91c6343ddc6
+size 8853

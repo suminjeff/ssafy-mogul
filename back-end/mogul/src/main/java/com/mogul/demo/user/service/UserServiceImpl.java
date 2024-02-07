20c3d5fd3b37d464edf5b17b42a4740e40f5f41d
신종욱 <rmm1@naver.com>
2024-02-07 18:17:38 +0900

    fix(profile): Update required value of user info update
    Former-commit-id: 24c798f4b1f3ac7138841ed3ea08e30358882318

--- back-end/mogul/src/main/java/com/mogul/demo/user/service/UserServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/service/UserServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c58a530719e16fb6057f59721ae2029d3a8d04ce71d2888c020e84da58233603
-size 4833
+oid sha256:b4d4c76fde7b00f511238d5ba566c20093fea4921fb44bc02889b4686522fec1
+size 4894

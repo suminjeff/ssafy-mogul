28a7fd53fd65fc73522accdf0d7ffb1101ffc0d6
신종욱 <rmm1@naver.com>
2024-02-07 17:02:30 +0900

    refactor(profile): Update DTO and controller
    Former-commit-id: e9a5b767efec2a331dd9a698687445603656dc7b

--- back-end/mogul/src/main/java/com/mogul/demo/user/controller/ProfileController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/controller/ProfileController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:720eb662d1ffe719c90668bb057fb3fa7199c9f73a445a2fe646be6f6ebeb8f5
-size 5341
+oid sha256:38bc59eb772ed344d5594d81f6938b170ebcfd1ec54e5691e36e182a92a601c8
+size 1827
---
+++ back-end/mogul/src/main/java/com/mogul/demo/user/dto/UserDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1935d3bba9da3d0a37f8815980db0997e4003c369378602f9a049f76d17712c0
+size 762

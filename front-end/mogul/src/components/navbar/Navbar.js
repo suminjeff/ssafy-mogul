c2d0455854f0874beca5ad705732794aa64e829a
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-07 17:33:32 +0900

    feat" Modify chat
    Former-commit-id: c13fedb2d13a3f99402c76d488c43bf9320c2715

--- front-end/mogul/src/components/navbar/Navbar.js
+++ front-end/mogul/src/components/navbar/Navbar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88388f48aa9992e934f57b30ff340f8198b6a0f329dd84abd7b57da0319e74d8
-size 3517
+oid sha256:b057f6515f2699fb080f7017bf9e043158f49d9c987330ffd6b9b04c5b490414
+size 3556

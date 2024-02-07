9915baf119510aa7a3155d1382022cbe1801e5fd
신종욱 <rmm1@naver.com>
2024-02-07 18:00:12 +0900

    fix(user): Add exception handling in userService
    Former-commit-id: 85475e3d2836f4b9ced8f95c9ca497737442c345

--- back-end/mogul/src/main/java/com/mogul/demo/user/service/UserService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ad1ff649b305e44084dc11f3752e8e00fd9bf3c0ee0f35e16c63026563bc824
-size 991
+oid sha256:edb9b89c9a4dbb44425aebd430c532fdd62c7b79ba04d26a21dd291ce25f3ebc
+size 1034

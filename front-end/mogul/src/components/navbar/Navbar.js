a6822f45af73fcecde691df7798cb3a9e67855f7
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-07 10:59:37 +0900

    feat: Modify navbar toggle
    Former-commit-id: fcf222f7c4a4e90afdcc4f78d8c081736acd433f

--- front-end/mogul/src/components/navbar/Navbar.js
+++ front-end/mogul/src/components/navbar/Navbar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbda005d801b7f0603c10aa2982c2871247ba84af1388a6bdc8fff501f017a53
-size 1746
+oid sha256:3b6a600e3b5945b488c2b2dff5015e3baa95900d8c9a749465a49725df33b0fa
+size 3448

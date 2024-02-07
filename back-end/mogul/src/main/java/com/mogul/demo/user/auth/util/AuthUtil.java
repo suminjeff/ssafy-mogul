231049fb051b3dbc4076b9fe7eba6ce34a50e766
신종욱 <rmm1@naver.com>
2024-02-07 16:46:03 +0900

    fix(profile): Update my library response
    Former-commit-id: ad926785fadf2f5ea1bf0fcf64977db1dfa332e2

--- back-end/mogul/src/main/java/com/mogul/demo/user/auth/util/AuthUtil.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/auth/util/AuthUtil.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2053b708fc6d0fa0c743d6d0e55fb839e3c5d7c84a48edd511fa0cd5aa62b10
+oid sha256:603a245f5cd5a7bc9177bf0e632e7261a988cdaff020f4ed7848919bcb9d90f5
 size 904
--- back-end/mogul/src/main/java/com/mogul/demo/user/controller/ProfileController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/controller/ProfileController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cb619b6036d1e749e2074eb5b2c04a4eb0c4162f5197978be1b93d38acd0d04
-size 5348
+oid sha256:f6655540f31bf2bf724f61a8a427ec51029e3d373f2e3f947dd259c90e78a4ce
+size 5351

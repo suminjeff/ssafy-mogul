78b1c25a5e43000ee087ab58bd54e1a830e0a071
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-02-07 14:53:50 +0900

    feat: Modify boardPagination
    Former-commit-id: 81d3944a707baa2b9f199377952088b5bfe562a5

--- front-end/mogul/src/app/board/[id]/BoardPagination.jsx
+++ front-end/mogul/src/app/board/[id]/BoardPagination.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5031e34ffe664717effa9415a65907faee5c172b516fddd5d445dfa286c2e208
-size 8175
+oid sha256:627107e0d99c636b74b8dbf6984d279cb91baf751620a27c6e013656b1f15759
+size 9024

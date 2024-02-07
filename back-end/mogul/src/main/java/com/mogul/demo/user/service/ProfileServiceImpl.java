5e2c3ed75bf2541f3c675d31c3d283bb80de6c3d
신종욱 <rmm1@naver.com>
2024-02-07 17:15:49 +0900

    fix(profile): Add @transactional to profile service
    Former-commit-id: 868260099991927d8a4c882b63c132703fb5fb3a

--- back-end/mogul/src/main/java/com/mogul/demo/user/service/ProfileServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/service/ProfileServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:caec98cb3e493728307bfdf0eaeeb86e6c2e68ea66b1236583ffe64689a9efeb
-size 1750
+oid sha256:f8c233614c4e26ba2487936473690f98d14f7acb02414bd21358bc6aab035bb2
+size 1802

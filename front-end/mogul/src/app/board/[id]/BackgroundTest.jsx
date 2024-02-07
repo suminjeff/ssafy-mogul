d09546e71d6fa5259277e6d1f470903d76c601b3
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-02-07 13:17:32 +0900

    feat(board): Remove some codes
    Former-commit-id: 792a4c7d72416719b3aecdaccbfba2ea6d98857e

--- front-end/mogul/src/app/board/[id]/BackgroundTest.jsx
+++ front-end/mogul/src/app/board/[id]/BackgroundTest.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7b404d6498eca094110cf5a48af36927a27a741677154ec414b09a04a5d0310
-size 846
+oid sha256:28d9a0eb88112baecdbfeb561f0a12a6cdf030980ef5d323d87405283defbb5b
+size 897
---
+++ front-end/mogul/src/app/library/List/DeleteMyLibrary.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9097b25d2957b824ff270e10c3b38f3e479b43e3bc11ddf0d075b3d439e7766a
+size 2642

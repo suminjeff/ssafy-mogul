d212d28efc8d6f20d573aad6f7f221ae652f8e8c
Sieun Kim <sieun043@gmail.com>
2024-02-07 12:11:27 +0900

    feat(webtoon):Update common library service library add
    Former-commit-id: c3f59810892983388071a433a8ac555c33c2a14a

--- back-end/mogul/src/main/java/com/mogul/demo/common/service/CommonLibraryService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/common/service/CommonLibraryService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16077aef9b79aed0db7bd0dd79935652f34215d8d54d439c8f45ed21476f2ecf
-size 199
+oid sha256:6c3cbab8a5bca924d131fad25ab33ef42700402f1f28cba61faa649f22a769d3
+size 320
--- back-end/mogul/src/main/java/com/mogul/demo/common/service/CommonLibraryServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/common/service/CommonLibraryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:324921fbe74db2ad55bbe178ec38a7bc17daf96271e275fb6bea8c91bf0849ce
-size 887
+oid sha256:2d5c58772f951df72702ae4a2d785028f75758dae3f1bdf4761b4e772c22b91c
+size 1337

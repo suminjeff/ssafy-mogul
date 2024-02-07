ab44b2d6198d46baaed2380791b74f871a902078
이수민 <suminjeff@gmail.com>
2024-02-07 11:14:23 +0900

    feat(mainpage): Modify mainpage
    Former-commit-id: 636c4d1553dd727c6998d6955d307fcbc89d56e1

--- front-end/mogul/src/app/webtoon/[id]/page.js
+++ front-end/mogul/src/app/webtoon/[id]/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:654011e224279b0fe0f0cc85efb02c1420e1c64c2126734e07c228de43fcad4d
-size 4183
+oid sha256:0ce1fa756347bb285a64f98a1444aefd49800569ee8a64eddf3499ad427a0abc
+size 4179

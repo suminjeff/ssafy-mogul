c94b20656823ab6165cbdcdcefa0bd55a909c5bd
On Yoo <dhsdb02@gmail.com>
2024-02-07 14:56:09 +0900

    feat(comment): Modify mapping
    Former-commit-id: f8dd2e2d1adbde194e7d068b0b0525ba1fddfbbf

--- back-end/mogul/src/main/java/com/mogul/demo/board/controller/CommentController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/controller/CommentController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8735e0fc6293f97fb197e24d85976489755cb9a26c7d64863f1612ebc3790538
-size 4650
+oid sha256:963e11b91ed15e93d84e426abddb5ab20329f8dabc7f05db4eefe9a28a6cb0b6
+size 4758

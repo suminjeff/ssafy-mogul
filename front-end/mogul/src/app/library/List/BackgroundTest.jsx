ae8bcb1e50a3460d86b150a997cac555cee6dd4c
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-02-07 14:27:44 +0900

    feat: Seperate libraryList
    Former-commit-id: 7dc16e868eb75408262cfef353b1c03baa40cced

--- front-end/mogul/src/app/library/List/BackgroundTest.jsx
+++ front-end/mogul/src/app/library/List/BackgroundTest.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06f31f7b8cf92312b8b0bed0b85ff58e9dcc5aef5831ee8240605dd6634920ee
-size 410
+oid sha256:f31e6addc9fca6f50c2dfcb8d77a5c6bb43b10bc271d8b1b12b97079b731c9bc
+size 456

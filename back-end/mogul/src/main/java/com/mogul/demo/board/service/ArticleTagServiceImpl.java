0ed784c75b06114975759f14d916bdce0fbb6bd6
On Yoo <dhsdb02@gmail.com>
2024-02-07 15:05:15 +0900

    feat: Add transactional
    Former-commit-id: 13af3d2bba01ad620056d83cc641190735c34057

--- back-end/mogul/src/main/java/com/mogul/demo/board/service/ArticleTagServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/service/ArticleTagServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48c9d68d629bf5947f29385048edf9b2a6bb92f10159854c71d7eebdbb36daef
-size 3816
+oid sha256:2dc618a0966fec10cffc4636ea27960b67da3eb78e5b1f609ad6000d493f0c4d
+size 4050
--- back-end/mogul/src/main/java/com/mogul/demo/board/service/CommentServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/service/CommentServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46ef4d0a8eb50e4dcabcb8d089702db6d04da478eb15f783149fb4abd2d55e24
-size 3487
+oid sha256:b1f3dd62028729fed1835deb2829395df78dcdeb88761ce8cc1674f6ff9ac6d9
+size 3520

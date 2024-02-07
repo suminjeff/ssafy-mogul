a1647aeb79b4d7a680be75ffa687342584741a9c
Sieun Kim <sieun043@gmail.com>
2024-02-07 10:31:17 +0900

    feat(webtoon):Add commonservice and Fix webtoon controller
    Former-commit-id: 264dc4192527c5deaab588183fd96bae16295310

--- back-end/mogul/src/main/java/com/mogul/demo/common/dto/WebtoonMainResponse.java
+++ back-end/mogul/src/main/java/com/mogul/demo/common/dto/WebtoonMainResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d517ff0163141778d2f62c7df328ab2261022a20b16b4a50d98c4db0822f66a5
-size 284
+oid sha256:49b8facd1c84d88ae6bf0053f998a3c2ce6d16c9df384d9ba5b13193867817eb
+size 369

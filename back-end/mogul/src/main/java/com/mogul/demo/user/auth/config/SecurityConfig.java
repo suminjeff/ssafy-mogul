3b01b3e05f1084714057b570cdfdb3bc0a0fa0a7
신종욱 <rmm1@naver.com>
2024-02-07 12:41:38 +0900

    fix(auth): Update authentication filtering
    Former-commit-id: 297d01974f923ae3fac98e239a4f28017d2e7b2d

--- back-end/mogul/src/main/java/com/mogul/demo/user/auth/config/SecurityConfig.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/auth/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:801f404ecce8ce2a61f62e2be74668fe0aef9d45f17eefb96c20f861c1970aab
-size 3784
+oid sha256:8b8dbae8004c042fa1528bb39e30c9ee587b045f3f03e518b953ee8a2f7026db
+size 3950

aadc1413e19f289d6d31430c0d5d7e746922f7ae
Sieun Kim <sieun043@gmail.com>
2024-02-07 10:47:18 +0900

    feat(webtoon):Add like remove
    Former-commit-id: 58ee9b7f6dc64c7276ef63097073f8ae89df225d

--- back-end/mogul/src/main/java/com/mogul/demo/common/service/CommonWebtoonService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/common/service/CommonWebtoonService.java
@@ -1,13 +1,3 @@
-package com.mogul.demo.common.service;
-
-import com.mogul.demo.common.dto.WebtoonDetailCommonResponse;
-import com.mogul.demo.common.dto.WebtoonMainResponse;
-import com.mogul.demo.webtoon.dto.WebtoonLikeResponse;
-
-public interface CommonWebtoonService {
-    WebtoonMainResponse listWebtoonMain(int pageNumber, int pageSize);
-
-    WebtoonDetailCommonResponse getWebtoonDetail(Long webtoonId, int pageNumber, int pageSize);
-
-    WebtoonLikeResponse getLike(Long webtoonId);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:8a0830c6ac129ab373f7799b17e5fe5ae4f8a6ffd990112f4a95dc2060b8a5f5
+size 551

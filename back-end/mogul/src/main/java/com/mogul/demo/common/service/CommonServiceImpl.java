3c31def86050f3d1a1c15543bfe8788c8bc2cfab
Sieun Kim <sieun043@gmail.com>
2024-02-07 10:28:24 +0900

    feat(webtoon):Add commonservice and Fix webtoon controller
    Former-commit-id: 09cc9f355fea83b26543e39c50fe4639a2729090

--- back-end/mogul/src/main/java/com/mogul/demo/common/service/CommonServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/common/service/CommonServiceImpl.java
@@ -1,7 +1,3 @@
-package com.mogul.demo.common.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class CommonServiceImpl implements CommonService{
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9d1a33bb331661630c5373f776b3f7ff8289b448aab9caf929c356a548bfdc3b
+size 820
--- back-end/mogul/src/main/java/com/mogul/demo/webtoon/controller/WebtoonController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/webtoon/controller/WebtoonController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:309ff826fd5f75d16da462b15dd7620cfb55ae8733745956180a1ba1e3cf82b0
-size 9397
+oid sha256:b4192b48e62fb773d385523bf2215c3ce4bb627a11417847f6ab17da40ed7780
+size 9392

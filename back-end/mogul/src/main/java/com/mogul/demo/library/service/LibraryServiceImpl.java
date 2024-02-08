2e07b3027ebad12df627a43c0ce5711a25f901d7
Sieun Kim <sieun043@gmail.com>
2024-02-08 23:47:32 +0900

    feat(library):Add limit to create library
    Former-commit-id: 116393ff9363027f4f9e087008726dd14b2992b6

--- back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/library/service/LibraryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5015109f279d27d6feb67ccff88db2eda57e90f7bbfaf6694a0cf7ff3faa8c4f
-size 7329
+oid sha256:64bfed397913a3cbaa498d41c4c8372fcba7eedb2cec006b1d0ecc5274f824c6
+size 7560

48eacc15776fe7d4b0896eb62c5156060143b1f0
신종욱 <rmm1@naver.com>
2024-02-08 14:28:34 +0900

    refactor(exception): Add exception for request with revoked token
    Former-commit-id: d280157f35d43b1c76010a16d7f857b908891ed6

--- back-end/mogul/src/main/java/com/mogul/demo/advice/GlobalExceptionHandler.java
+++ back-end/mogul/src/main/java/com/mogul/demo/advice/GlobalExceptionHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45a7bc24fd448bfce5bcaf8679a6929269a6a240a8678979a6760f8c64d349ed
-size 2537
+oid sha256:95dc67b6c56802d35fa79a520f644881e870a7f687d469741415aec25765d3ab
+size 2877

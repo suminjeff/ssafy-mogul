20fb15533b6e15015bdb152cf44f6d0e7db669ca
이수민 <suminjeff@gmail.com>
2024-02-08 13:25:48 +0900

    feat(webtoon): Modify webtoon main
    Former-commit-id: b15905f0e86ab24550c104cd63923845280353b2

--- front-end/mogul/src/app/user/profile/ProfileInfo.jsx
+++ front-end/mogul/src/app/user/profile/ProfileInfo.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d799bd9301ec9fcdd87067b6b14620f41a28989231b4ab10df93c266e4365eff
-size 2920
+oid sha256:6bc6d8c14c06ef299c43382eed7602c7f64566d9784c7b0e76463e1bcbcc6a36
+size 2424

d4674744fa3489727a412a2f377c031215cec21b
onyoo <dhsdb02@gmail.com>
2024-02-10 01:34:24 +0900

    feat(search): Add webtoon search domain & repository
    Former-commit-id: e9d7adca407af96c44d550228742feb37a910d70

---
+++ back-end/mogul/src/main/java/com/mogul/demo/search/dto/WebtoonSearchResponse.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e429bbbb5d2a4bdf7e2ad137858b2c0bedfa15e1409f41965d76caef26f4a086
+size 253
---
+++ back-end/mogul/src/main/java/com/mogul/demo/search/mapper/WebtoonDocumentMapper.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:89ec55e1ed702819e85b1098f229f0b3dadb23b1634e5d66a1fb4d73b72ac06f
+size 450

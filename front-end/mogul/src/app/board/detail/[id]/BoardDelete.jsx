d70f697feef2e54a8355645be3830edfaa51e8e2
백천주 <dieswing@naver.com>
2024-02-09 18:58:03 +0900

    feat(library): isValidUser
    Former-commit-id: ea9fdc849f2c5a27cc201740341115f60c22af54

--- front-end/mogul/src/app/board/detail/[id]/BoardDelete.jsx
+++ front-end/mogul/src/app/board/detail/[id]/BoardDelete.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76c66a1c3bb1290f6a8d2a85e92ee64de55f2cce52db8eefb7e0a3f460027ef7
-size 1409
+oid sha256:5bb488d65c3922ed5ec38c33a5f93c204bd76835ecc71e9498421aac611e9bbd
+size 1424
--- front-end/mogul/src/app/library/List/BackgroundTest.jsx
+++ front-end/mogul/src/app/library/List/BackgroundTest.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:819105fdd1c22a5775ffd1c5a829d6d56f2561abf4f1238061fa2389f1d4f9c4
-size 971
+oid sha256:d1eb710d53e2615f615ddd2e1e372bc69c479d8573b5560e769b71f6bf9db833
+size 887

4a2561ec83bf0799175f9cb3c5d6366e782c075e
이수민 <suminjeff@gmail.com>
2024-02-10 15:35:05 +0900

    feat(webtoon): Modify review
    Former-commit-id: 186a630946d63ad2202ec60bfacb21835f286588

--- front-end/mogul/src/components/webtoon/review/ReviewCreateForm.jsx
+++ front-end/mogul/src/components/webtoon/review/ReviewCreateForm.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e11924d6ed3a998ec10b43ae896007e773ef1e51f819c2897f4c56604a5321fb
-size 4189
+oid sha256:c00f70af33d1c5e5d65469003f9a58ca504929ab337eeb4cb8ea8cc8ec266548
+size 4126

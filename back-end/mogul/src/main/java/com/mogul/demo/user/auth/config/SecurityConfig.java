d65789d595daa152a621e0d54c357d835be03861
신종욱 <rmm1@naver.com>
2024-02-11 01:16:00 +0900

    fix(auth): Update CORS filter
    Former-commit-id: ff5afa634b878f4267dfda20509e181ca4cd7437

--- back-end/mogul/src/main/java/com/mogul/demo/user/auth/config/SecurityConfig.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/auth/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c26156aeff4a7b9a185a8eea5e5086b99edff27153837cc21a9c85d020b4b8aa
-size 4165
+oid sha256:9bbd423e7c0a1e40273da58f0e70b32058700b44f060225a9e6e5b71c90fa453
+size 4210

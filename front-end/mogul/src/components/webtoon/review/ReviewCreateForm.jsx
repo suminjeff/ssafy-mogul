55a35228d6449713c563b26c8537a684ab7a7d93
이수민 <suminjeff@gmail.com>
2024-02-10 15:25:30 +0900

    feat(webtoon): Add review function
    Former-commit-id: 1e70c13ae2c1af1d3cdfb1a42f1d26feaf34bfea

--- front-end/mogul/src/components/webtoon/review/ReviewCreateForm.jsx
+++ front-end/mogul/src/components/webtoon/review/ReviewCreateForm.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d57c4a74b2ab22cf13bff7bfe97887e4154d6a4f0a1d519fd90c4be95b0e87f7
-size 5086
+oid sha256:e11924d6ed3a998ec10b43ae896007e773ef1e51f819c2897f4c56604a5321fb
+size 4189
--- front-end/mogul/src/components/webtoon/review/ReviewWrapper.jsx
+++ front-end/mogul/src/components/webtoon/review/ReviewWrapper.jsx
@@ -2,7 +2,6 @@ import ReviewCard from './ReviewCard';
 
 function ReviewWrapper({ reviews }) {
   const firstFiveReviews = reviews.slice(0, 5);
-
   return (
     <div className='mx-10 my-20'>
       <div className='grid grid-cols-5 gap-10 mx-10'>

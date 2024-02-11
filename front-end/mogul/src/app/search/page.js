20d0a399272da97acc928b5ac591bfd0c6dfbb9a
백천주 <dieswing@naver.com>
2024-02-11 22:40:29 +0900

    feat(search): Add searchPage and fixError
    Former-commit-id: 428b2821e8023d7b54d1fd64a9f74d011c148bad

---
+++ front-end/mogul/src/app/search/page.js
@@ -0,0 +1,12 @@
+import Navbar from "@/components/navbar/Navbar";
+
+export default async function SearchPage() {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div className="text-center mt-20">검색페이지입니다.</div>
+    </div>
+  );
+}

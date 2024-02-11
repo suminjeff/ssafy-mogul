74ee93b257f6b01461b54b9b05a3ab6f7a0d133d
yeongbin05 <bigstar96115@hanmail.net>
2024-02-12 06:39:29 +0900

    feat: Modify comment delete authentication
    Former-commit-id: 53223a6f105419f11d60c5ccff4420ee4d49ef2d

--- front-end/mogul/src/app/board/detail/[id]/BoardCommentCreate.jsx
+++ front-end/mogul/src/app/board/detail/[id]/BoardCommentCreate.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:011e363bc9b42a2e7e47fdc31e89171b5114a566b6bb3f09c5fffa12944fda00
-size 2347
+oid sha256:982dbe4035d084f0dd23d593425434132456243d9ca283741033a6c9fb712cb4
+size 2166
--- front-end/mogul/src/app/board/detail/[id]/BoardDetail.jsx
+++ front-end/mogul/src/app/board/detail/[id]/BoardDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0783800d907817dd580443e176075c5ecbfabe5262c94b440f788a8d82ca7432
-size 5921
+oid sha256:82e14a453d2666aca8a12c8d0dbfaf2c7e64ee207ffbadfdc9f83c0f2410e126
+size 6008

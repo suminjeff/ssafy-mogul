cae0bc23c573b186343a032342a1dae25c435b52
이수민 <suminjeff@gmail.com>
2024-02-11 21:57:39 +0900

    feat(webtoon): Some bugs fixed
    Former-commit-id: d9985e25ab95345574cd5e0b060183069e43684b

--- front-end/mogul/next.config.mjs
+++ front-end/mogul/next.config.mjs
@@ -3,7 +3,10 @@ const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["www.kmas.or.kr"],
+    domains: [
+      "www.kmas.or.kr",
+      "dn-img-page.kakao.com",
+    ],
   },
   // async rewrites() {
   //   return [
--- front-end/mogul/src/app/webtoon/[id]/page.js
+++ front-end/mogul/src/app/webtoon/[id]/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1dc509c7b961bd6496c983eddc8c385c2c8485cfe0b4a9eb08f3fa7276536aa9
-size 2114
+oid sha256:985b90ab2fcd6c866b740a169062c030a4aba45ca451787309bfad20ec021962
+size 2108

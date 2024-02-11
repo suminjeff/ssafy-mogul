9a7fbd2a6ec7fec9c5793338c907e6549d762a7f
백천주 <dieswing@naver.com>
2024-02-11 21:20:47 +0900

    feat(profile): Modify MyBoard
    Former-commit-id: a1ae3f5ce628db69fde3cfa55616375827e476e2

--- front-end/mogul/next.config.mjs
+++ front-end/mogul/next.config.mjs
@@ -1,18 +1,3 @@
-/** @type {import('next').NextConfig} */
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["www.kmas.or.kr"],
-  },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: `/api/user/signup`,
-  //       destination: `/api/user/login`,
-  //     },
-  //   ];
-  // }
-};
-
-export default nextConfig;
+version https://git-lfs.github.com/spec/v1
+oid sha256:65c09e9b31a0e37db5cbce1c8ad3d9d71c814584cfb8348c0544235ec3f9eac6
+size 502

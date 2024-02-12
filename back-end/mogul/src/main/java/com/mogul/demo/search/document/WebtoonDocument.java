9bc80018eba447c1e82e3055483f6489e36430c9
On Yoo <dhsdb02@gmail.com>
2024-02-13 08:37:10 +0900

    refactor(search): Modify index name
    Former-commit-id: 2edd47347592dee2c20af0ca43e4d2e367e59225

--- back-end/mogul/src/main/java/com/mogul/demo/search/document/WebtoonDocument.java
+++ back-end/mogul/src/main/java/com/mogul/demo/search/document/WebtoonDocument.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:730de9b590d7984b5ef4badffa4f6590f0a85e29adcfbea4b5ec19e9fb455886
+oid sha256:5042a2b70985b5bf808c03dd8afa87f20d3ae3db7d0d602c7d27d0a842d71412
 size 1463

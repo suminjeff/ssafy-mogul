0633c994a2fcbf9105dae54d5780eaa9f7191976
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-02-13 13:49:15 +0900

    feat(library): Seperate response
    Former-commit-id: 750bc1a714f022a9caedc61b123ca98225d99f46

--- front-end/mogul/src/app/library/[id]/SubscribeThisLibrary.jsx
+++ front-end/mogul/src/app/library/[id]/SubscribeThisLibrary.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:830836198abd4abeaa718dfeb6e10f6cae6e606e651409e6bf0b64493a6da8b4
-size 1488
+oid sha256:97ff411d89ac04dd2103bcca9e39472fc1c1b8d68d4af9f104b16dacf8509096
+size 1704

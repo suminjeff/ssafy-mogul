d44954fe1325f181ea2a6e567d9826aa19985e94
백천주 <dieswing@naver.com>
2024-02-13 09:35:50 +0900

    Update front-end/mogul/src/app/library/list/[id]/BackgroundTest.jsx, front-end/mogul/src/app/library/list/[id]/DeleteMyLibrary.jsx, front-end/mogul/src/app/library/list/[id]/LibraryCreateModal.jsx, front-end/mogul/src/app/library/list/[id]/LibraryList.jsx, front-end/mogul/src/app/library/list/[id]/MySubscriptionList.jsx, front-end/mogul/src/app/library/list/[id]/SkeletonLibrary.jsx, front-end/mogul/src/app/library/list/[id]/page.js, front-end/mogul/src/app/library/list/BackgroundTest.jsx, front-end/mogul/src/app/library/list/ChangeLibraryName.jsx, front-end/mogul/src/app/library/list/DeleteMyLibrary.jsx, front-end/mogul/src/app/library/list/IsNotLogin.jsx, front-end/mogul/src/app/library/list/LibraryCreateModal.jsx, front-end/mogul/src/app/library/list/MyLibraryList.jsx, front-end/mogul/src/app/library/list/page.js
    Former-commit-id: 65bc3ed617516f7c925fbf94d9715a601cce9b26

---
+++ front-end/mogul/src/app/library/list/[id]/page.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f3eab4144f307597406b9805dff5fd389ba3f409bb08cfdd2d651fd54ed04356
+size 287
---
+++ front-end/mogul/src/app/library/list/page.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e94029c4da6a31d9a8de25f7b297de8dd07560776bcddcd654f920104e167c0c
+size 294

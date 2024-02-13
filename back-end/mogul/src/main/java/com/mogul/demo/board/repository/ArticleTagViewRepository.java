cfecb6f58bdf20716bd403ec5cb480ee626234d7
On Yoo <dhsdb02@gmail.com>
2024-02-13 09:54:52 +0900

    feat(search-article): Add  article search dto
    Former-commit-id: c8973082b7460fcd2c1da3a85962db25d7beee9c

--- back-end/mogul/src/main/java/com/mogul/demo/board/repository/ArticleTagViewRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/board/repository/ArticleTagViewRepository.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9cb0bd2661387f26e46ffb65d389a4efdfa579504cfee47f07deae685eb7d06e
-size 385
+package com.mogul.demo.board.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mogul.demo.board.entity.ArticleTagView;
+
+public interface ArticleTagViewRepository extends JpaRepository<ArticleTagView,Long> {
+
+	List<ArticleTagView> findArticleTagViewById(Long id);
+	List<ArticleTagView> findArticleTagViewByTag(String tag);
+}
--- back-end/mogul/src/main/java/com/mogul/demo/search/dto/WebtoonTotalSearchResponse.java
+++ back-end/mogul/src/main/java/com/mogul/demo/search/dto/WebtoonTotalSearchResponse.java
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7e41bccefe4761c452149f1f8a78274a4bfb0509bd0f2f4141564ed41aad15ca
-size 241
+package com.mogul.demo.search.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class WebtoonTotalSearchResponse {
+	List<WebtoonSearchResponse> title;
+	List<WebtoonSearchResponse> genre;
+	List<WebtoonSearchResponse> summary;
+}

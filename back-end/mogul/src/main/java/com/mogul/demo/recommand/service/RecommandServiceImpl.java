3b7454ca941ce2184c82e77502ae2ca82d092373
Sieun Kim <sieun043@gmail.com>
2024-02-13 16:57:57 +0900

    feat(recommand):Delete unused import
    Former-commit-id: 87ab1835f3316ce4cdc7e0dc019327a4e17bde41

--- back-end/mogul/src/main/java/com/mogul/demo/recommand/service/RecommandServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/recommand/service/RecommandServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:025103adc0d3303a3ccf48503fbaadf53aa9d1cb345c92e0930bb52d67c643ee
-size 1858
+oid sha256:5ce991891be41d170ced07d109f19d4a52e5ba8ed21d80af7cf3480327e55fa1
+size 1806

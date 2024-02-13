9e8b512bcf39e05226b5198c7afa7ec5eb7ef085
신종욱 <rmm1@naver.com>
2024-02-13 16:40:57 +0900

    refactor(user): Delete unused parameters and Add Swagger docs
    Former-commit-id: e3c6bd1d641e47e97ef8438c37d1321e2583ab33

--- back-end/mogul/src/main/java/com/mogul/demo/user/controller/ProfileController.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/controller/ProfileController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de13ebff368322c1f0c07ddd66c445ef3fae8a71800bc4a8362b652bfb2f4afa
-size 1845
+oid sha256:c61664b828129ba13daa311865b8e3a8c8b3f830109e4d93fd44cb16f54fe068
+size 2722

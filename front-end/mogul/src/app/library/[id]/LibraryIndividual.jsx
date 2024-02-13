cf6281bf7d9e0a5bc8e3582436173680ef830b40
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-13 16:46:18 +0900

    feat: Modify library authentication
    Former-commit-id: a439c287a676d7ce9d717a4cd4eb3b30296e5fd5

--- front-end/mogul/src/app/library/[id]/LibraryIndividual.jsx
+++ front-end/mogul/src/app/library/[id]/LibraryIndividual.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25957b41ae46cbbb73d2a9b9b2ec17ff87cefe5b376ba04096c71e7e3eba648a
-size 3192
+oid sha256:c1759cd7f2011ee29700722a91bb3f47dd227a6f152835f9ce5e8efeea08284d
+size 4371

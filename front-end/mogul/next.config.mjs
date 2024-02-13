b5919953c418d3c8acb1c0eafad027838c6619fd
이수민 <suminjeff@gmail.com>
2024-02-13 11:22:42 +0900

    fix: Config image remotePatterns
    Former-commit-id: b49eaeade7ccd94115a044ecc63d69efc05e7035

--- front-end/mogul/next.config.mjs
+++ front-end/mogul/next.config.mjs
@@ -1,3 +1,23 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:505d73aa415549745fc8f2d0e1000646e4fbcf14ea6adb068a2c24b0b498efe7
-size 660
+/** @type {import('next').NextConfig} */
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: `/api/user/signup`,
+  //       destination: `/api/user/login`,
+  //     },
+  //   ];
+  // }
+};
+
+export default nextConfig;

04b01fd93c703ffcd20c31ae1a43c83c5a4f0022
On Yoo <dhsdb02@gmail.com>
2024-02-14 08:05:54 +0900

    feat(recommend): Add elastic client request
    Former-commit-id: d9777c44b75aea04fda53181a5c5eb514e2bbec7

--- back-end/mogul/src/main/java/com/mogul/demo/recommend/service/RecommendService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/recommend/service/RecommendService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b77eaf805a3ff1a7bf81ccb1661243ca65cf6411c104b44b96a422fb891a07ff
-size 234
+oid sha256:0b427f165dd143846a812d29de0670abf42f23668611abee15aa400a545d0015
+size 563
--- back-end/mogul/src/main/java/com/mogul/demo/recommend/service/RecommendServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/recommend/service/RecommendServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca1f57afba3406c8918ba21d61c169ba42de62daabf754fd8e8608f9064fb42e
-size 1540
+oid sha256:59fccf426a05dda9f1c56fa38858fe90532c36486a1a9b0aa8357e38e83300ea
+size 4459

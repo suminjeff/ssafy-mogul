06081924e534e22b06e337e7ac35937db2162bd3
Sieun Kim <sieun043@gmail.com>
2024-02-13 16:05:50 +0900

    feat(recommand): Add Recommand service
    Former-commit-id: 9169a77918f30514f453596baad16791a4280aa5

---
+++ back-end/mogul/src/main/java/com/mogul/demo/common/service/CommonRecommandServiceImpl.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8bc291a19f33b6f6c0b2b7cc2dbb46f54b3fb51f87452df9f5720637b9677811
+size 1661
--- back-end/mogul/src/main/java/com/mogul/demo/review/service/ReviewService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/review/service/ReviewService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9144509f1d243e554b9ec3eb877b3a2ffb39e5d1b1a1d6b2e48e86b4f0e93cfe
-size 624
+oid sha256:dcb52385d51820b9f29d5d398bf9c0012f5e992a12fff64b83b86e9c530465a1
+size 678

8291fab79f3c0362c3d84362b0d05b0c1b5ab8f5
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-13 11:33:19 +0900

    feat: Modify Library
    Former-commit-id: 7940f1ad439b7773a6164e6d13241d4b6d4bb636

--- front-end/mogul/src/app/board/create/BoardCreate.jsx
+++ front-end/mogul/src/app/board/create/BoardCreate.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b9387aeba18f51da6eb6dcc8566b1c541a137f011d92608df7614cd9c0ad68e
-size 3796
+oid sha256:74a40d22715bf5580df65303b8e2e4fe2fa2febc257b354431ba00d168ae6fa2
+size 3840

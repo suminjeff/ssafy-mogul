bb8c0b79d28fb1c85c3cf86fdb8dfa4b1f48899d
이수민 <suminjeff@gmail.com>
2024-02-13 16:20:54 +0900

    feat(search): Add search result page
    Former-commit-id: ca4b8048a356b0d39d570e5e339725dba78665e8

--- front-end/mogul/src/components/footer/Footer.js
+++ front-end/mogul/src/components/footer/Footer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88369b5e7ed9ab372469da95a577bd5fbe3ec10d732dc7579e4b1e8d91bd3ad6
-size 1948
+oid sha256:ac7793eb64c19fa1e17e4025b5e657fdb6f219bb89d32d6402f4f9861ec9d7a2
+size 1981

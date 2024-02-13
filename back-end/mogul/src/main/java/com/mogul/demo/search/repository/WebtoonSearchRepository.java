e4ed71dbabd4b030d652aafcdc17a5b6afd04dd5
On Yoo <dhsdb02@gmail.com>
2024-02-13 13:11:35 +0900

    feat(search-webtoon): Modify spacing issue
    Former-commit-id: 91a8a68b0040794abdbba64b5e8086fb40a02d77

--- back-end/mogul/src/main/java/com/mogul/demo/search/repository/WebtoonSearchRepository.java
+++ back-end/mogul/src/main/java/com/mogul/demo/search/repository/WebtoonSearchRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:caef9363e77f976c87ff994fac67a442e8b8db7273c662d972f97f91b8f9ba1a
-size 589
+oid sha256:78b7b83467c09bfdc9d858db8a37c5dd32149e75c89edea65f726af949838cc1
+size 821

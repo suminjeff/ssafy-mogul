8a3ed2b07537150a79e86abb43aad3078047e512
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-02-13 13:49:15 +0900

    feat(library): Seperate response
    Former-commit-id: 33c9de412dc879a70d6adab499c01590ce40b896

--- front-end/mogul/next.config.mjs
+++ front-end/mogul/next.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:505d73aa415549745fc8f2d0e1000646e4fbcf14ea6adb068a2c24b0b498efe7
-size 660
+oid sha256:df50d2c615b608fe2c9702d4a09f428c1dbbec350e9ed174551b33365307bafe
+size 839

6ad7f74c257d2f16c633c597dfe048bfa26014f1
Sieun Kim <sieun043@gmail.com>
2024-02-13 16:57:11 +0900

    feat(recommand): Add Recommand Service
    Former-commit-id: 20845d6778ba06fcd0632227a895f939a722918e

--- back-end/mogul/src/main/java/com/mogul/demo/recommand/service/RecommandService.java
+++ back-end/mogul/src/main/java/com/mogul/demo/recommand/service/RecommandService.java
@@ -1,9 +1,3 @@
-package com.mogul.demo.recommand.service;
-
-import com.mogul.demo.common.dto.RecommandResponse;
-
-import java.util.List;
-
-public interface RecommandService {
-    List<RecommandResponse> ListRecommandWebtoons(Long webtoonId);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:77f8c60a4f2500a5162e18b7e31bf7e61e00c802cfdd5f8d51e4365cbe81dd3e
+size 286

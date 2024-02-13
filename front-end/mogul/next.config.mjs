b89f4c4473f1f2b2aa8ae94e594991e053db405c
이수민 <suminjeff@gmail.com>
2024-02-13 11:22:42 +0900

    fix: Config image remotePatterns
    Former-commit-id: 22cb2fa0a69a28a513e592863fa34cf6a0f93b30

--- front-end/mogul/next.config.mjs
+++ front-end/mogul/next.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:505d73aa415549745fc8f2d0e1000646e4fbcf14ea6adb068a2c24b0b498efe7
-size 660
+oid sha256:e5da56699801e65d2e9485118607f9f5b95cda611185247b4ccaa3d6c99b0678
+size 443

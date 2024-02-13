d4d94db4b1e387ddd8b48664c713bb1e5e9db514
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-13 17:38:11 +0900

    feat: Modify library CSS
    Former-commit-id: 09079a9c67dd6243ec2da1b0119d15a1f3692954

--- front-end/mogul/src/app/library/[id]/MyWebtoonThumbnailCard.jsx
+++ front-end/mogul/src/app/library/[id]/MyWebtoonThumbnailCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f790b353ec818d0c27f9705c4b5c06b9704b535f16d4ecffb6f49a7bbac89724
-size 1692
+oid sha256:185b098fecaca42419c2def00c39c474c246c3f76f7697d5de30ffe9445f0781
+size 1701

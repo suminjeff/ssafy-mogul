ed02fc458cfc065d45dd451cba74a5623c49b4b0
On Yoo <dhsdb02@gmail.com>
2024-02-14 08:04:03 +0900

    feat(recommend): Add recommend dto
    Former-commit-id: 50f038dc70faeff74fa0c02f8c9a4780d08f4be1

---
+++ back-end/mogul/src/main/java/com/mogul/demo/recommend/dto/WebtoonInfo.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5863d5ae3de8a7a37e5152c55d0faed05f70624b7a7c84955cde006d1c9ae7d4
+size 242
---
+++ back-end/mogul/src/main/java/com/mogul/demo/recommend/dto/WebtoonRecommendResponse.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a209988ac491078e628069bcd049f697604218c5584469ff6c97f1ed0916a3d0
+size 331

233cd2db52ffb030b00abae3bf5e20196d650d6c
이수민 <suminjeff@gmail.com>
2024-02-13 10:55:54 +0900

    feat(webtoon): Modify review wrapper from ssr to csr
    Former-commit-id: e633731c96abcad90f707514a0d08dfffb50d59d

--- front-end/mogul/src/app/webtoon/[id]/page.js
+++ front-end/mogul/src/app/webtoon/[id]/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:985b90ab2fcd6c866b740a169062c030a4aba45ca451787309bfad20ec021962
-size 2108
+oid sha256:c91434b29816fead3e9ebb3a76e400625b44e3678e6614769e74ee973149a28b
+size 1787

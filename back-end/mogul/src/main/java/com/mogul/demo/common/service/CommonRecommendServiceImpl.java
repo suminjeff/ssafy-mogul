1846906801dbdc3f939a546c87d8211ea9d12a02
On Yoo <dhsdb02@gmail.com>
2024-02-14 08:06:21 +0900

    feat(recommend): Modify return type
    Former-commit-id: 2e5ccd0db40a9da0c57d79e51d1bfb66a90754c8

--- back-end/mogul/src/main/java/com/mogul/demo/common/service/CommonRecommendServiceImpl.java
+++ back-end/mogul/src/main/java/com/mogul/demo/common/service/CommonRecommendServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ef341f0c26cc2ee85f6a319f6e8485526d93780b834d47651873be2134fe244
-size 1550
+oid sha256:80c290abc885ec6ab36333ff06ef649d4c0aef96f4a098b4c6b3d4360048b0af
+size 1601

67fcbebbbf423224e8b0db1d4760d7792ebbefcb
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-14 17:40:24 +0900

    feat: Add recommend base
    Former-commit-id: 8bde2f36a6399d9cf53c38a1aba912e6a6909277

--- front-end/mogul/src/app/recommend/page.js
+++ front-end/mogul/src/app/recommend/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f500453cbcb1b6e2512978cdbe0b0066a82822b43fdad278e3d8f59638987436
-size 1527
+oid sha256:7a9c3eddeb976f3131ae0409dd0d07356c6a227816eb5d7d65f2bc661e7928ad
+size 5395

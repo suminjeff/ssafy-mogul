ad04df109a25f99264e1a9d87022a9f50a89e0a7
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-14 10:42:11 +0900

    feat: Modify library card css
    Former-commit-id: 897f206af2522dc4dcb725121b9faa38b4dacf33

--- front-end/mogul/src/app/library/LibraryPopular.jsx
+++ front-end/mogul/src/app/library/LibraryPopular.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad2dae9167e5c0ae98ab6e46a49e08d37a36e44a815a39f4719aef097ac84f6f
-size 6933
+oid sha256:b6eb0b3641867a9b7937acb93f7fe032e64d3fb632c7a65213ff281596c09ec5
+size 6992

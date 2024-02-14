9d3075291ca3dc007253c18978e4b69437994715
이수민 <suminjeff@gmail.com>
2024-02-14 11:15:45 +0900

    feat(webtoon): Add review details
    Former-commit-id: d91b38196799b9bd0f7babccc1362d5836e9279d

--- front-end/mogul/src/app/webtoon/[id]/review/page.js
+++ front-end/mogul/src/app/webtoon/[id]/review/page.js
@@ -1,12 +1,3 @@
-import Navbar from "@/components/navbar/Navbar";
-import Header from "@/components/webtoon/header/Header";
-import ReviewWrapperAll from "@/components/webtoon/review/ReviewWrapperAll";
-export default async function WebtoonReviewAll(props) {
-  return (
-    <>
-      <Navbar />
-      <Header heading={"이 웹툰의 리뷰"}/>
-      <ReviewWrapperAll/>
-    </>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5f63487419783b5d9c2cbe2200da238820b1e70abfa7ffd64b845019da5671ff
+size 1314

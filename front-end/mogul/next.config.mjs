1d810d3b67d1dd06e6bdac1e87c6195789d5ab07
이수민 <suminjeff@gmail.com>
2024-02-14 14:37:18 +0900

    hotfix: Fix config
    Former-commit-id: b6a607234662a44ec1b24da403393707cc185d9a

--- front-end/mogul/next.config.mjs
+++ front-end/mogul/next.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3cef31dcfca1ea2d3e8ce7ccd93e00a23b5422422720c84a350a2f1357978cc
-size 255
+oid sha256:d36265e3f5aad39e34d2c6bb74a2a6750445630b815868ee0d9b0556fe54ac7a
+size 258

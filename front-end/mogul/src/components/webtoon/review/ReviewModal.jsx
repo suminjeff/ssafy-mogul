f2b8aa861b26b54ae63ef944ca98d5d7d5a639dd
이수민 <suminjeff@gmail.com>
2024-02-14 16:23:33 +0900

    hotfix: Fix images again
    Former-commit-id: ca5e46ee89bb8de1e70ec57e6d735272d008b3d7

--- front-end/mogul/src/components/webtoon/review/ReviewModal.jsx
+++ front-end/mogul/src/components/webtoon/review/ReviewModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f3b9a1aaccbf19cdc2007d22cb9a04127aeacc8fd069c43a7dd29d7b3d6d375
-size 1656
+oid sha256:15880761303495d1f1564af31fd1af643975758bf75308c9de2143de0ca9a465
+size 1715

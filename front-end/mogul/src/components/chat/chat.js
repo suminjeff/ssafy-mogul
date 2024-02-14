02f37e2268e8958e1434aa85392640b5dbebf4c5
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-14 15:17:32 +0900

    feat: Modify websocket url
    Former-commit-id: 2db155dcf86e1bac54ab2f1df6a23af4449addf6

--- front-end/mogul/src/components/chat/chat.js
+++ front-end/mogul/src/components/chat/chat.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ede52d51587d1f0ecd9254756a74dd5beba4dcc5b6fe565ed1a1ce257e03744
-size 3322
+oid sha256:0379f241f911d91c8287b6b02b68b833442e0d8408c11547f67b75b62a70103c
+size 3328

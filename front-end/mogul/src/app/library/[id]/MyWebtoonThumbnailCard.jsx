a0102c895eef7001d39c63cd09a85e2b11be243c
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-14 10:24:52 +0900

    feat: Modify mylibrary pen
    Former-commit-id: 66b97d52faeb2ee984ead916518a4c594a4384e4

--- front-end/mogul/src/app/library/[id]/MyWebtoonThumbnailCard.jsx
+++ front-end/mogul/src/app/library/[id]/MyWebtoonThumbnailCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:185b098fecaca42419c2def00c39c474c246c3f76f7697d5de30ffe9445f0781
-size 1701
+oid sha256:5b24850d43b50b3ded21452e54592282bbcace382cfefa54c3d710140880e699
+size 2853

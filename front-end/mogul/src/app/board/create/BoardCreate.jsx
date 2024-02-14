f47cf1cc8ce0153b3a3293a7cb3a1c76c719a279
lookoblong <139312980+lookoblong@users.noreply.github.com>
2024-02-14 13:01:20 +0900

    feat(board): Add boardCSS
    Former-commit-id: 24c6a8c299fe175cd9b793a419b466f0ed661a3f

--- front-end/mogul/src/app/board/create/BoardCreate.jsx
+++ front-end/mogul/src/app/board/create/BoardCreate.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c79781313dfb3acec1a8af221c5ff290098b0b3a592f1b0fede032675eae6b5
-size 4040
+oid sha256:8e5a642f3eebc121970889a21864cc9f19f01167f04c60089f80f8e2c273243a
+size 4161
--- front-end/mogul/src/app/user/profile/preview/MyComment.jsx
+++ front-end/mogul/src/app/user/profile/preview/MyComment.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:035c2470fe13c6f68bd0e8d7989d18be95dc6987b123ee4c27b265aae172d3bc
-size 3940
+oid sha256:c98e2935af80c00307777f9c912ad077a1bfc44438426b55106845f9204db627
+size 3969

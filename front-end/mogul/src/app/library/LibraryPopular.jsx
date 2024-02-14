a023352eaf2931168ae0e1251db7be567db6b65c
이수민 <suminjeff@gmail.com>
2024-02-14 14:51:43 +0900

    hotfix: Fix Image to img
    Former-commit-id: f1ec2935b102be397f66d175e3ea979e27a1344e

--- front-end/mogul/src/app/library/LibraryPopular.jsx
+++ front-end/mogul/src/app/library/LibraryPopular.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6eb0b3641867a9b7937acb93f7fe032e64d3fb632c7a65213ff281596c09ec5
+oid sha256:814b0dc80fdb3f3ce74531a30a70c07be557d62f8b2110e29710d0f1a6dc0148
 size 6992

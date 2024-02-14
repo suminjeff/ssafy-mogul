896ebb8561bbcb68c12d65d8428d524a9bed1886
이수민 <suminjeff@gmail.com>
2024-02-14 15:02:40 +0900

    hotfix: Fix Image to img server images
    Former-commit-id: 63d4d0deba7a1f953e144edafc0cb2da5f977c9a

--- front-end/mogul/src/components/webtoon/WebtoonThumbnailCardSearch.jsx
+++ front-end/mogul/src/components/webtoon/WebtoonThumbnailCardSearch.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d4c5908ffa48e20c157fa21ce7ea1739f4c3c1a8dd4ada5b30ce136965b9c8d
-size 1558
+oid sha256:0a0a8b9ba7329500c3a169cd5874080ddaf70d60b1eed061b6da56f6f07fd7e0
+size 1559

7633963c71a151427194cf3f6bd741469220649a
이수민 <suminjeff@gmail.com>
2024-02-14 17:40:26 +0900

    feat(chat): Add chat component
    Former-commit-id: 4463d698b430b50dff62712ce1e7775a88df8b1b

--- front-end/mogul/next.config.mjs
+++ front-end/mogul/next.config.mjs
@@ -1,15 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // basePath: '/public',
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
-};
-
-export default nextConfig;
+version https://git-lfs.github.com/spec/v1
+oid sha256:d36265e3f5aad39e34d2c6bb74a2a6750445630b815868ee0d9b0556fe54ac7a
+size 258

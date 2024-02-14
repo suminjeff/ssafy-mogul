5c898fa44d3d065b125a099226a531fe846341e9
Sieun Kim <sieun043@gmail.com>
2024-02-14 11:13:36 +0900

    fix(recommend): Delete unused class
    Former-commit-id: dff5d5fc492c59d59ea3f04a3064ee78531ee9e5

--- back-end/mogul/src/main/java/com/mogul/demo/recommend/document/WebtoonEmbedding.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cb5a8dfc1a1e171d9d7d5badd81b45ad6de61423e7a680882d9afe62cffa70f4
-size 622
--- back-end/mogul/src/main/java/com/mogul/demo/search/repository/WebtoonEmbeddingRepository.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:622c0c6428f4fdd4c63b6b441ca09bcd1ec31f53d5e1654b064072038cd407a1
-size 646

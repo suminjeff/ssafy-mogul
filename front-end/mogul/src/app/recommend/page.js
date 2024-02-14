16d1d4dfcfbc777f58b89a65d9e670a6f0bf42dc
yeongbin05 <bigstar96115@hanmail.net>
2024-02-15 07:28:45 +0900

    feat: Modify recommend
    Former-commit-id: 5c50257d9f5b37bd18f85eb5419b813085309dd3

--- front-end/mogul/src/app/recommend/page.js
+++ front-end/mogul/src/app/recommend/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b556afb830b10a2d8f362ce25b351e3f74b608643347ebcf16ba81ddb10bc7e5
-size 7562
+oid sha256:9cb2957e780ccf6ac0502851b750c0451303ce9c69c85b6dd726d86181a49bd8
+size 4645

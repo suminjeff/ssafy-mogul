9052f282bd412f47e0540f86205048daea4c40d8
Sieun Kim <sieun043@gmail.com>
2024-02-14 15:23:49 +0900

    fix(chat): Update cors filter
    Former-commit-id: d33f6dd62881bdd568517829a6f1436cc6a4be45

--- back-end/mogul/src/main/java/com/mogul/demo/user/auth/config/SecurityConfig.java
+++ back-end/mogul/src/main/java/com/mogul/demo/user/auth/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4d1f29052b878b09848112d316796686ebcd311f1bebac478c226155a647962
-size 4340
+oid sha256:2e32c537f4c5f034bae790e8e0c0ac42137df82629c9abc7a609ec5b7f88044e
+size 4442

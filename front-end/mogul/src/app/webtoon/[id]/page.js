3c9fdabb40fe6049e4b8b9169ed9a371e3dccb30
이수민 <suminjeff@gmail.com>
2024-02-14 10:34:53 +0900

    feat(search): Fix some bugs
    Former-commit-id: 30d716cb924ce2d0301bb313b98093e39973abc0

--- front-end/mogul/src/app/webtoon/[id]/page.js
+++ front-end/mogul/src/app/webtoon/[id]/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c91434b29816fead3e9ebb3a76e400625b44e3678e6614769e74ee973149a28b
-size 1787
+oid sha256:21e4e871cb3cce9a1511de9a9f4130f718d7668ae38466bc4efc7ab46512550e
+size 1817

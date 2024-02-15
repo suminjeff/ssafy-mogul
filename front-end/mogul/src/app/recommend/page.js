6182ebb3a680cacce281ba1121a7a58053bfdbeb
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-15 11:00:24 +0900

    feat: Modify chat scroll
    Former-commit-id: ba2a26fc7089aedcf3b4288bd113cda210d0b40c

--- front-end/mogul/src/app/recommend/page.js
+++ front-end/mogul/src/app/recommend/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cb2957e780ccf6ac0502851b750c0451303ce9c69c85b6dd726d86181a49bd8
-size 4645
+oid sha256:1ea7a4749e74c05e7e6f9e8488094fb9dc8d0345027c28cbdfe08df58f238e3f
+size 4742

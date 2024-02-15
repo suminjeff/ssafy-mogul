09155f48d4fcdabe787b58c6e9c979de09730129
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-15 16:16:07 +0900

    feat: Modify error
    Former-commit-id: b163175748c5899428a046739193e69beb5d78fb

--- front-end/mogul/src/app/board/BackgroundTest.jsx
+++ front-end/mogul/src/app/board/BackgroundTest.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:739ac1583b068f2b1e44c0aadd7258826c52dd592cb83b7e10f9b1a18483090c
-size 1003
+oid sha256:dcdd379639247058cbb2ffe645f4681c67962a626a30e8dd5b99fc561964f022
+size 1009

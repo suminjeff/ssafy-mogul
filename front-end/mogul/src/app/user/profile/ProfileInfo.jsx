54c4b38061b8a87584b9e873778187733a5c4919
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-15 10:05:02 +0900

    feat: Add recommend button
    Former-commit-id: 09745a0b318f1b5d46ed92fac0424cb427f26038

--- front-end/mogul/src/app/user/profile/ProfileInfo.jsx
+++ front-end/mogul/src/app/user/profile/ProfileInfo.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:686467ca4683e05c5d67f4c37cd5e5cee76b63534f103907cb2bb3caff2ab86f
-size 5926
+oid sha256:86ec2abdab083773203b42c8bef1ec2d5abb200b96355b495036aaec0a3dde97
+size 6077
--- front-end/mogul/src/components/webtoon/WebtoonDetailCard.jsx
+++ front-end/mogul/src/components/webtoon/WebtoonDetailCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2193380ed48754c245a6f8c524b48a07f165e70e807941c0d8339da520b3c9c
-size 3344
+oid sha256:6045b88c34531d2f8c7cf13bcace85e7452f64c60e526c3779540294fca352db
+size 3582

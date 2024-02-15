3c892c86f3df231644c60ee4d8d0d2c54e5390c7
이수민 <suminjeff@gmail.com>
2024-02-15 16:38:55 +0900

    hotfix: Add chat style
    Former-commit-id: 85333ed4efe63a398c0986dcaf4c8b9f5b3d08e1

--- front-end/mogul/src/components/chat/ChatApp.js
+++ front-end/mogul/src/components/chat/ChatApp.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c66fb8b1ebc47493df4071488bef35acc405aae8d69e33a1321afdba268b689d
-size 4592
+oid sha256:aae9134f4c4fc6326804f7192191c0b02be6ea2eb76281889c9bd889f17fcf58
+size 5273

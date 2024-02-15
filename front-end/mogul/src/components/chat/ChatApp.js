5e1c9be5bbcd337719930f40e1506ddd7755ebb7
이수민 <suminjeff@gmail.com>
2024-02-16 00:09:28 +0900

    hotfix: Is user logged in interface
    Former-commit-id: 184c51a917d5d1058db6413793850be984dba072

--- front-end/mogul/src/components/chat/ChatApp.js
+++ front-end/mogul/src/components/chat/ChatApp.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52cb78265bcdbb411e490498bb7d1ef5451487b77d0b55e35fab1a5302c583af
-size 5356
+oid sha256:df4de387cea6f9dae9b7599f4b62495c155d2d7f6a12c7936498e4fe47c124c5
+size 5359
--- front-end/mogul/src/components/navbar/Navbar.js
+++ front-end/mogul/src/components/navbar/Navbar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d894b85b24f6c5349294707d6d7ec2aece44bbd6f7a94a4b050b2a55d3a9b55
-size 4799
+oid sha256:8cd975b524e56eb94a5e3f16760de53390065ee46b34d154cef7f2c83877b1fe
+size 5046

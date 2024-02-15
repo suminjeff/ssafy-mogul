fa089ed9318cf3885e6b2baa28d4be546ea41684
이수민 <suminjeff@gmail.com>
2024-02-15 10:05:01 +0900

    feat(chat): Add chat function
    Former-commit-id: 23979dbf1550352ca4be13aa5c484886d9d0d417

--- front-end/mogul/src/components/footer/Footer.js
+++ front-end/mogul/src/components/footer/Footer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9558c05cdd3610bcedc87ffd863b9981c012977214f15f035ecd965a67d09f49
-size 2083
+oid sha256:d15676fdf6513d4b38c50e07f08172d57c3f4ad4e6ef9af549bb04705fb8eb2f
+size 2074

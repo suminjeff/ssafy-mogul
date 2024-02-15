41160eb8d4ac1c864419cc8e0569992f28eb3aa7
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-15 17:46:08 +0900

    feat: Modify recommend radius
    Former-commit-id: d47e4de63b42b030a31a6290591d3154c444251f

--- front-end/mogul/src/app/recommend/page.js
+++ front-end/mogul/src/app/recommend/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8aa79e90b432409e20d3b05d6fafa1e7622e215d5d89e6c5eff1ee2c059df41a
+oid sha256:d3a4a89e865762307e18cd202875fb0b5921912ad14c18999f35fbd686d3676f
 size 6425

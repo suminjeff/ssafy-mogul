f916993f5688f4b4f094c485db7a84fed6d4ba75
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-02-15 11:33:43 +0900

    feat: Modify img tag
    Former-commit-id: 562c76641243b71cf8135ab5cd800d4b814013ca

--- front-end/mogul/src/components/webtoon/webtoonLibrary/WebtoonLibraryComponent.jsx
+++ front-end/mogul/src/components/webtoon/webtoonLibrary/WebtoonLibraryComponent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b77ff0e27fee2193d8f419d5ac2fd5cdc83212ed67649c58eb7208a1bcabeb0
-size 2401
+oid sha256:8547707676cae2d945d31be8b9c04485bf01c15d5b365c5aea6219a91e196283
+size 2393
